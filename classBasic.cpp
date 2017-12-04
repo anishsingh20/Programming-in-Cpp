96448c7d90f7d5ac8fc8bb53a4072e025122b4e6
anishsingh@20 <loveeanish@gmail.com>
2017-12-05 00:23:22 +0530

    basic OOP

---
+++ classBasic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+class Human {
+	string name;
+	
+};
+
+string Human :: name="Anish";
+
+
+int main()
+{
+   Human anish;
+   anish.name;
+	
+return 0;	
+}	
