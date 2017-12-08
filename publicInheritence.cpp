dedcbc82ce7a56a0f685afac3ef7094cf4f2b446
anishsingh@20 <loveeanish@gmail.com>
2017-12-08 23:42:56 +0530

    Public inheritence in cpp

---
+++ publicInheritence.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+class Shape {
+	
+	protected:
+		int side;
+		
+	public:
+		
+		int getArea();
+		int volume();
+};
+
+
+//PUBLIC inheritence-public members of parent class act as public members of child class 2) Protected member of parent class, as protected member of child class
+
+class square : public Shape {
+	public:
+		//constructor
+		square(int a) {
+			side=a;
+		}
+			
+		
+		//inherited from Shape(base) class
+		void getArea() {
+			
+		cout<<"Area is: "<<side*side<<endl;
+		}
+		void volume() {
+			cout<<"Volume is: "<<(side^4) << endl;
+		}
+		void perm() {
+			cout<<"Perimeter is: "<<(side*4)<<endl;
+		}
+};
+
+
+int main () {
+	
+	square s(4); //object of subclass
+	s.getArea();
+	s.perm();
+	s.volume();
+}
