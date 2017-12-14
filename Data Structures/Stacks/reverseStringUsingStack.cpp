e413ef2c237b1e8d8f76f124ebdc1540d8fd8fab
anishsingh@20 <loveeanish@gmail.com>
2017-12-15 02:41:10 +0530

    demonstrated 2 different implementation to reverse a string

--- Data Structures/Stacks/reverseStringUsingStack.cpp
+++ Data Structures/Stacks/reverseStringUsingStack.cpp
@@ -1,28 +1,58 @@
 #include<iostream>
 #include<string>
+#include<stack>
 
 using namespace std;
 
 //reversing a string using stack- TIME COMPLEXITY AND SPACE COMPLEXITY= O(n) , n = size of string
 
-void reverse(string str) {
-	
-	int i = 0;
-	int j = str.length()-1;
-	
-	
-//	if(i==j) {
-//		return;
+//void reverse(string str) {
+//	
+//	int i = 0;
+//	int j = str.length()-1;
+//	
+//	
+////	if(i==j) {
+////		return;
+////	}
+//	
+//	while(i<j) {
+//		
+//		swap(str[i],str[j]); //swapping 
+//		i++;
+//		j--;
 //	}
+//	
+//	cout<<str; //now str is updated with new reversed value	
+//}
+//
+//int main() {
+//	string name;
+//	getline(cin,name);
+//	reverse(name);
+//	
+//	return 0;
+//}
+
+
+
+//------------------------USING STACK--------------------
+
+void reverse(string str) {
+	stack<char> s;
 	
-	while(i<j) {
-		
-		swap(str[i],str[j]); //swapping 
-		i++;
-		j--;
+	//push string to stack
+	//reading the string char one by one and pusing it to stack
+	for(int i=0;i<str.length();i++) {
+		s.push(str[i]);
 	}
 	
-	cout<<str; //now str is updated with new reversed value	
+	//popping the chars and revrrsing
+	for(int i=0; i<str.length();i++) {
+		str[i] = s.top(); //storing top element of stack in str,reversal
+		s.pop(); //popping
+	}
+	cout<<str; //string is now updated and reversed
 }
 
 int main() {
@@ -32,3 +62,4 @@ int main() {
 	
 	return 0;
 }
+
