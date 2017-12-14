8631e6033c484deb9187108b2a7979de411d49aa
anishsingh@20 <loveeanish@gmail.com>
2017-12-14 13:38:46 +0530

    Implemented Vowels or not using char pointer

---
+++ VowelsorNotusingPointer.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	
+	char *ptr;
+	char name[100];
+	ptr = name; //ptr now stores address of name array, array's label itself acts as an address of the array
+	int countv=0,countc=0;
+	
+	//will use the pointer to do all the ops, as char pointer and char array act as same thing
+	
+	gets(ptr);//reading the char
+	
+	for(int i=0;ptr[i]!='\0';i++) 
+		{
+			
+		if(isalpha(ptr[i])) { //only check for alphabets
+			
+			if(ptr[i]=='a'|| ptr[i]=='e' || ptr[i]=='i' || ptr[i]=='o' || ptr[i] =='u') {
+				
+				countv++;
+			}
+			else {
+				countc++;
+			}
+		}
+	}
+	cout<<"The number of vowels are: "<<countv<<endl;
+	cout<<"Number of consonants are: "<<countc<<endl;
+	return 0;
+	
+}
