da516b9220a34381a4bd3b58b71c87ee3270090c
anishsingh@20 <loveeanish@gmail.com>
2017-12-17 15:47:18 +0530

    added another solution to merge 2 LL in sorted order

--- Data Structures/LinkedLists/Merge2LLSorted.cpp
+++ Data Structures/LinkedLists/Merge2LLSorted.cpp
@@ -1,12 +1,12 @@
-/*
-  Merge two sorted lists A and B as one linked list
-  Node is defined as 
+
+ // Merge two sorted lists A and B as one linked list
+  //Node is defined as 
   struct Node
   {
      int data;
      struct Node *next;
   }
-*/
+
 //RECURSIVE SOLUTION to MERGING 2 SORTED LINKED LISTS
 Node* MergeLists(Node *headA, Node* headB)
 {
@@ -33,5 +33,60 @@ Node* MergeLists(Node *headA, Node* headB)
       
    return(temp);   
   
+}
+
+//-----------------------------SOLUTION USING REFERENCES and ITERATIVE SOLUTION--------------------
+
+
+Node* MergeList(Node *headA, Node *headB) {
+	Node *new_head=NULL;
+	Node *temp; //the ned dummy variable which will keep track of the sorted list
+	
+	if(headA==NULL) {
+		return headB;
+	}
+	else if(headB==NULL) {
+		return headA;
+	}
+	
+	
+	//if data in node A < data in node B then assign temp to the smaller node
+	if(headA->data <= headB->data) {
+		temp=headA;
+		headA = temp->next; //head A moves to the next of temp
+	}
+	else {
+		temp=headB;
+		headB=temp->next; //headB moves to next of temp
+	}
+	 
+	new_head=temp;// now we need to assign new_head to temp node which is the node with smallest value
+	//this new_head will be returned by the function
+	
+	while(headA && headB) {
+		
+		if(headA->data <= headB->data)  {
+			temp->next=headA;
+			temp=headA; 
+			headA=temp->next;
+		}
+		else {
+			temp->next=headB; //temp links to headB
+			temp=headB;//temp becomes headB node
+			headB=temp->next;//headB moves to next to temp
+		}
+		
+	}
+	
+	if(headA==NULL) temp->next=headB; //when List A is finished
+	if(headB==NULL) temp->next=headA; //when list B is finished
+	
+	return(new_head); //will return the new header of the sorted merged LL
+	
+	
+	
+	
+	
+	
 }
 
