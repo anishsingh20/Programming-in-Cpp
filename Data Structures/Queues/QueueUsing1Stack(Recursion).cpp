842902806b14a5ac0c723f1e58a7888a5b4d224c
anishsingh@20 <loveeanish@gmail.com>
2017-12-18 11:26:24 +0530

    implemented queue using 1 stack and recursion

---
+++ Data Structures/Queues/QueueUsing1Stack(Recursion).cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<stack>
+
+// Date- 18/12/17 10:46
+using namespace std;
+
+//queue using 1 stack and recursion(function call stack)
+
+stack<int>S1;
+
+class Queue{
+	public:
+		void Enqueue(int data);
+		int dequeue();
+		
+};
+
+
+void Queue::Enqueue(int data) { //simply push into stack S1
+	
+	
+	//simply push data into stack s1
+	S1.push(data);
+
+
+	
+		
+}
+
+
+
+int Queue::dequeue() {
+	
+	int res,x,y;
+	if(S1.empty()) {
+		cout<<"Queue is Empty..."<<endl;
+		return -1;
+	}
+	
+	
+	//case when stack has only one element
+	if(S1.size()==1) { //if stack has only one element
+		
+		x = S1.top();
+		S1.pop();
+		
+		return x;
+		
+	}
+	
+	else { //recursively call dequeue()
+		
+		y = S1.top();
+		S1.pop();
+		
+		res = dequeue(); //res stores the last popped element
+		
+		//push everything back to stack
+		
+		S1.push(y);
+		
+		return res; //returning the last popped item
+		
+		
+	}
+}
+
+
+
+
+
+int main() {
+	
+	Queue q; //creating object of class Queue
+	int choice,data;
+	
+	
+	while(1){
+		cout<<"\n1)Insert\n2)Delete\n3)Exit\n"<<endl;
+		cin>>choice;
+		
+		
+		switch(choice) {
+			
+			case 1: 
+				cout<<"Enter data :"<<endl;
+				cin>>data;
+				q.Enqueue(data);
+				break;
+			case 2:
+				
+				cout<<q.dequeue();
+				break;
+			
+			
+			default:
+				exit(0);
+				break;
+				
+		}
+	}
+	
+return 0;
+		
+}
+
+
