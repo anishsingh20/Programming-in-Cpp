abc0ec86698589a6bf43daf248572c5a1ae0e988
anishsingh@20 <loveeanish@gmail.com>
2017-12-21 01:15:12 +0530

    implemented finding size of a variable uwithout using sizeof operator

--- Pointers/sizeofVarWithoutUsingSizeof.cpp
+++ Pointers/sizeofVarWithoutUsingSizeof.cpp
@@ -2,23 +2,60 @@
 
 using namespace std;
 
+
+//a function to print the size in bits
+//int size()
+//{
+//  int a = 1;
+//  int size = 1;
+//  while(a<<=1)
+//    size++;
+//  return size;
+//}
+
+
 int main () {
 	
-	int *ptr,**ptr2, a=10;
-	ptr=&a;//ptr stores address of a
-	ptr2=&ptr; //ptr2 stores address of ptr, *ptr now print value at ptr
-	//-ptr++;
+	 long double a;
 	
-	cout<<"Address of a "<<&a<<endl;//address of a
-	cout<<"Value of var a i.e *ptr = "<<*ptr<<endl; //value of var a, where ptr is referencing/pointing to as ptr stores the address of var a.
-	cout<<"Address of ptr :i.e &ptr= "<<&ptr<<endl;//address of ptr in memory
-	cout<<"value stored in ptr, ie address of a,ptr =  "<<ptr<<endl;
-	cout<<"Address of ptr2 : &ptr2= "<<&ptr2<<endl;// address of **ptr2 in memory
+	//calculate the address difference between two consecutive int variables- the difference would be the size of that variable type
 	
-	cout<<"Value of ptr i.e address of a -*ptr2 = :"<<*ptr2<<endl;//addrerss of ptr where ptr2 is pointing.
-	cout<<"value stored in ptr2, i.e address of ptr "<<ptr2<<endl;
+	cout<<"Address of a :"<<&a<<endl;
+	cout<<"Address of a+1,i.e address of a incremented by 4 bytes :" << &a+1<<endl;//ie address of a incremented by 4 bytes
+	int sizeof_int = (char*)((&a)+1) - (char*)(&a);
+	cout<<sizeof_int<<endl;
+	
+
+//----------------METHOD-2-USING ARRAYS------------------
+
+cout<<"Size of a variable by method 2- "<<endl;
+long double arr[2];
+int size1 = ( (char*)&arr[1]- (char*)&arr[0] );
+
+cout<<size1<<endl;
+
+//cout<<"Size in bits :"<<size()<<endl;	
+	
+//NOTE-Also only character type and arrays are guaranteed to be laid out continously in memory, i,e memory allocated to them is contigious memory alloc
+/*a+1 is a pointer to a[1], and a decays to a pointer to a[0]. So it calculates the difference in chars between a pointer to a[1] and a pointer to a[0]. 
+
+That is, the number of chars occupied by a[0].
+
+Ss it necessary to type caste that to char *,I mean why can't we use int * for that? 
+  	 	
+If you use int* then the result will be 1. It shouldn't be surprising that (a+1) - a == 1.
+
+
+
+*/
+
+
+
+
+
+
+
 	return 0;
-	
 	
 	
 }
