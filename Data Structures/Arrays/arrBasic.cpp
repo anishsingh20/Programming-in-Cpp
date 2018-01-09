8cd2686c49e8da106e5d4c04a3b7750e3bade2a6
anishsingh@20 <loveeanish@gmail.com>
2018-01-09 23:29:56 +0530

    added some explanation in arrays

--- Data Structures/Arrays/arrBasic.cpp
+++ Data Structures/Arrays/arrBasic.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 int main() {
-	int arr[5]; //one contigious memory block allocated to the int array at compile time
-	//arrays are automatically passed by reference so , the name of the array prints the base address of the array -or add of 1 element of array
+	int arr[5]; 
+	/*one contigious memory block allocated to the int array at compile time
+	 arrays are automatically passed by reference so , the name of the array prints the base address of the array -or add of 1 element of array
 	
-	//Elements of array can be accessed in constant time at a specific location by using the subscript notation eg-arr[0] 	
+	Arrays are defined as contiguous blocks of memory, and so any array element will be physically near its neighbors in RAM. This
+	greatly benefits from modern CPU caching methods.
+	
+	Elements of array can be accessed in constant time at a specific location by using the subscript notation eg-arr[0] 
+	*/
+		
 	for(int i=0;i<5;i++) {
 		
 		
