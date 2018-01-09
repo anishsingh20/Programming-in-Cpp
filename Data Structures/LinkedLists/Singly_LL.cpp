44f6da625a765db3474601d971f7cddfbfd23eae
anishsingh@20 <loveeanish@gmail.com>
2018-01-10 02:28:49 +0530

    insertion function made

--- Data Structures/LinkedLists/Singly_LL.cpp
+++ Data Structures/LinkedLists/Singly_LL.cpp
@@ -11,7 +11,7 @@ struct Node {
 Node *head;
 
 /*BASIC OPERATIONS- 1)INSERTION-Insertion at beginning-O(1) , insertions at end-O(n) , insertion in middle-O(n)
-		    2)DELETION
+		    2)DELETION-front,tail and intermediate positions
 		    3)TRAVERSAL	-O(n)
 */
 
@@ -29,6 +29,38 @@ int listlength() {
 	
 	return count;
 }
+
+
+void InsertList(int data, int pos) {
+	
+	Node *temp,*temp1,*new_node;
+	
+	temp=head;
+	new_node->data = data;
+	
+	if(pos==1) { //insertion at beginning- update the head and next pointer of new_node
+		
+		new_node->next=temp;
+		head=new_node;
+		
+	}
+	
+	int k=1;
+	else {
+		//this condition will work for both insertion at middle and insertion at last
+		while(temp!=NULL) && while(k<pos) {
+			k++;
+			temp1=temp; //temp1 is the node previous temp node
+			temp=temp->next;
+		}
+		
+		temp1->next=new_node;
+		new_node->next=temp;
+	}
+}
+
+
+
 
 
 
