befb9147ed0e4fd5ea1c4d9c84da0df52b5bb617
anishsingh@20 <loveeanish@gmail.com>
2018-01-14 11:25:11 +0530

    program for implementing Doubly linked list

---
+++ Data Structures/LinkedLists/DoublyLinkedList.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+
+//Doubly linked list program
+using namespace std;
+
+struct Node{
+	string data;
+	Node* next,prev;
+};
+
+
+struct Node* head;//global declaration
+
+
+class DLL{
+	public:
+		void InsertPos(int pos);
+		
+		void traverse(struct Node *head);
+		
+		int delPos(int pos);
+		
+};
+
+void DLL::InsertPos(int pos) {
+
+}
+
+void DLL::traverse(struct Node *head) {
+	
+}
+
+
+int DLL::delPos(int pos) {
+	
+	
+	return
+}
+
+
+int main() {
+	
+	return 0;
+}
