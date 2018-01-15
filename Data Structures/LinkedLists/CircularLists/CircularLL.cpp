7ceb859401894b8b166cc0f854f02f64668c776d
anishsingh@20 <loveeanish@gmail.com>
2018-01-16 00:41:09 +0530

    function to add at head

--- Data Structures/LinkedLists/CircularLists/CircularLL.cpp
+++ Data Structures/LinkedLists/CircularLists/CircularLL.cpp
@@ -12,6 +12,8 @@ typedef struct CLLNode {
 
 
 //1)Function to traverse the circular linked list and return the count of elements
+
+//TIME COMPLEXITY =  O(n) , SPCAE COMPLEXITY = O(1) , for creating a temp node 
 int traverseCLL(struct CLLNode* head) {
 	CLLNode* curr=head;
 	int count= 0;
@@ -28,6 +30,62 @@ int traverseCLL(struct CLLNode* head) {
 	} while(curr!=head);
 	
 	return count;
+}
+
+
+//2) Function to insert a node at head
+void InsertFirst(CLLNode **head,int data) {
+	struct CLLNode* new_node = new CLLNode();
+	struct CLLNode* tail = *head;
+	
+	new_node->data= data;
+	//initially new node next points to itself
+	new_node->next = new_node;
+	//if list is empty
+	if(head==NULL) {
+		*head= new_node;
+		return;
+	}
+	
+	new_node->next = *head;
+	//traversing the list till end to update the tail and connect it with the new node
+	while(tail->next != *head) {
+		tail=tail->next;
+	}
+	tail->next = new_node;
+	//now head is new node
+	*head = new_node;
+	return;	
+}
+
+
+
+	
+	
+	
+	
+	
+}
+
+
+//3)Function to insert at tail of CLL
+
+void InsertLast(struct CLLNode** head,int data) {
+	
+}
+
+
+//4) Deleting a node at head
+
+void DelFirst(struct CLLNode **head) {
+	
+}
+
+
+//5)Deleting a node at tail of CLL
+
+void DelLast(struct CLLNode** head){
+	
 }
 
 
