c3f4005cd86c8d653acc11ab95664aa485333b9f
anishsingh@20 <loveeanish@gmail.com>
2018-01-28 14:46:06 +0530

    recursive solution to check a prime no

--- General Programs/PrimeNo.cpp
+++ General Programs/PrimeNo.cpp
@@ -3,32 +3,57 @@
 using namespace std;
 //function to check whether a number is prime or not
 
-bool Isprime(int n) {
+//bool Isprime(int n) {
+//	
+//	//prime number- a number which is only divisible by itself and 1
+//	//The definition of a prime number is a positive integer that has exactly two positive divisors-which is 1 and the num itself. 
+//	//However, 1 only has one positive divisor (1 itself), so it is not prime
+//	if(n <= 0) {
+//		
+//		
+//		cout<<"Enter a valid integer"<<endl;	
+//		return false;
+//	}
+//		
+//	for(int i = 2 ; i<=n/2 ; i++) {
+//	
+//		//if divisible by 2 or a multiple of 2
+//		if(n%i==0) {
+//			
+//			cout<<"Not a prime"<<endl;
+//			return false;
+//			break;
+//		}
+//	}
+//	cout<<"Is a prime"<<endl;
+//	return true;	
+//}
+//TIME COMPLEXITY =  O(n)
+
+//recursive solution
+int Isprime(int n ,int i=2) {
 	
-	//prime number- a number which is only divisible by itself and 1
-	//The definition of a prime number is a positive integer that has exactly two positive divisors-which is 1 and the num itself. 
-	//However, 1 only has one positive divisor (1 itself), so it is not prime
-	if(n <= 0) {
+	if(n > i) {
 		
-		
-		cout<<"Enter a valid integer"<<endl;	
-		return false;
-	}
-		
-	for(int i = 2 ; i<=n/2 ; i++) {
-	
-		//if divisible by 2 or a multiple of 2
-		if(n%i==0) {
+		if(n%i!=0) {
 			
-			cout<<"Not a prime"<<endl;
-			return false;
-			break;
+			return Isprime(n,++i);
+			
+			
+		}
+		
+		else {
+			cout<<"Not a prime"<<endl; 
+			return 0;
+			
 		}
 	}
+	
 	cout<<"Is a prime"<<endl;
-	return true;	
+	return 1;
+
+	
 }
-//TIME COMPLEXITY =  O(n)
 
 
 
