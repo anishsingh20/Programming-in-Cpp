21c5bb7e29df67204fcffb0e114c6d54aa4af94a
anishsingh@20 <loveeanish@gmail.com>
2018-01-28 23:27:04 +0530

    LCM of 2 numbers using HCF

--- General Programs/LCM.cpp
+++ General Programs/LCM.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 //function to find LCM of 2 numbers
-//The LCM of two integers n1 and n2 is the smallest positive integer that is perfectly divisible by both n1 and n2 (without a remainder)
+//The LCM of two integers n1 and n2 is the smallest positive integer that is perfectly divisible by both n1 and n2 (without a remainder). ie is a multipe of n1 and n2
 
 //eg- LCM of 72 and 120 = 360
 
@@ -12,13 +12,31 @@ using namespace std;
 //GCD= greatest common divisor/HCF
 //The HCF or GCD of two integers is the largest integer that can exactly divide both numbers (without a remainder).
 
-void LCM(int a,int b) {
+int LCM(int a,int b) {
 	
+	int gcd,lcm;
+	//first finding HCF
+	for(int i = 1; i<=a && i<=b; i++){
+		
+		//checking is i divides a and b
+		if(a%i==0 && b%i==0) {
+			gcd = i;
+				
+		}
+	}
 	
+	lcm = ( (a*b)/gcd );
+	
+	return lcm;
 	
 }
 
 int main() {
+	
+	
+	int lcm = LCM(5,10);
+	
+	cout<<"The LCM is:"<<lcm;
 	
 	return 0;
 }
