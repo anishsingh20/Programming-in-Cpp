0481e1e8b23a118bbcc5657f228a9fe7fc99afe8
anishsingh@20 <loveeanish@gmail.com>
2018-01-31 03:30:35 +0530

    implemented randomized quick sort to sort a vector

--- Data Structures/SORTING/QuickSort.cpp
+++ Data Structures/SORTING/QuickSort.cpp
@@ -42,13 +42,31 @@ int Partition(vector<int> &arr,int start, int end) {
 	return Pindex;//now at the end Pindex is the index of the PIVOT
 }
 
+
+
+/*To avoid the worse Case i.e when rearrangements results in unbalanced partitions on left and right of PIVOT
+Complexity of Quick sort which results in T(n) = O(n^2) , we use randomized quick sort algorithm, in which
+we can select any random element of the array as PIVOT element. This reduces the prob of worst case to 0 , and increases the probability of 
+average case complextiy i.e O(nlogn)
+*/
+int RandomizedPartition(vector<int> &arr,int start, int end)  {
+	
+	//choosing a random number between start and end index of array as Pivot index
+	int Pindex = start  + ( std::rand() % ( end - start + 1 ) );
+	
+	swap(arr[Pindex],arr[end]);
+	
+	return Partition(arr,start,end);
+	
+}
+
 //passing address of a vector as argument
 void QuickSort(vector<int> &arr,int start, int end) {
 	
 	
 		if(start>=end) return;
 		
-		int Pindex = Partition(arr,start,end); //calling partition function to rearrange the elements of array in such a way that 
+		int Pindex = RandomizedPartition(arr,start,end); //calling partition function to rearrange the elements of array in such a way that 
 		// left half has <= PIVOT elements, right half elements > PIVOT.
 		
 		//recursively calling Quicksort on left half of array
