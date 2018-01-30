b2455e1bc234300c7714d4ed2e14f27412da4131
anishsingh@20 <loveeanish@gmail.com>
2018-01-31 00:36:30 +0530

    Quick sort better than Merge sort answered

--- Data Structures/SORTING/QuickSort.cpp
+++ Data Structures/SORTING/QuickSort.cpp
@@ -9,7 +9,14 @@ QUICK sort is the best sorting approach for all practical problems, due to its e
 Quick sort is an IN-PLACE sorting algorithm, i.e it requires constant amount of extra memory ,sorting without any extra space requirement.
 Also in worst case scenario of O(n^2) is often ignored due to the RANDOMIZED VERSION OF QUICK SORT which gives the highest probability of O(nlogn)
 
+Another thing is that it does not requires to create new AUX arrays like in Merge Sort to divide the problem into subproblems. It does all the arrangements in the
+same array itself. This makes it save some extra memory as compared to MERGE SORT
+
 */
+
+
+
+
 int main() {
 	
 	
