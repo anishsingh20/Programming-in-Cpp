ceeb29a3ebf0d3f582c77c6896515972288390ff
anishsingh@20 <loveeanish@gmail.com>
2018-01-31 00:34:26 +0530

    Quick sort description added

--- Data Structures/SORTING/QuickSort.cpp
+++ Data Structures/SORTING/QuickSort.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 
 using namespace std;
-//PROGRAM FOR QUICK-SORT
-//TIME COMPLEXITY- WORST CASE - O(N^2) , BEST CASE - O(nlogn)
-//ALGORITHMIC STRATEGY- DIVIDE AND CONQUER
+/*PROGRAM FOR QUICK-SORT
+TIME COMPLEXITY- WORST CASE - O(N^2) , BEST CASE/AVG CASE - O(nlogn)
+ALGORITHMIC STRATEGY- DIVIDE AND CONQUER
+
+QUICK sort is the best sorting approach for all practical problems, due to its efficiency.
+Quick sort is an IN-PLACE sorting algorithm, i.e it requires constant amount of extra memory ,sorting without any extra space requirement.
+Also in worst case scenario of O(n^2) is often ignored due to the RANDOMIZED VERSION OF QUICK SORT which gives the highest probability of O(nlogn)
+
+*/
 int main() {
 	
 	
