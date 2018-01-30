a1b05d2153e06fd365adb5eec21982b8fe490f02
anishsingh@20 <loveeanish@gmail.com>
2018-01-31 00:31:27 +0530

    program to implement quick sort

---
+++ Data Structures/SORTING/QuickSort.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+//PROGRAM FOR QUICK-SORT
+//TIME COMPLEXITY- WORST CASE - O(N^2) , BEST CASE - O(nlogn)
+//ALGORITHMIC STRATEGY- DIVIDE AND CONQUER
+int main() {
+	
+	
+	return 0;
+}
