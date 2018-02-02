2129cebb91a968ab56228456c4e08a8a12bea8fb
anishsingh@20 <loveeanish@gmail.com>
2018-02-03 01:55:26 +0530

    implemented Brute force solution to find merge point

--- Data Structures/LinkedLists/MergePointofLL.cpp
+++ Data Structures/LinkedLists/MergePointofLL.cpp
@@ -8,6 +8,7 @@ struct Node {
 	int data;
 };
 
+
 //METHOD a) BRUTE FORCE-comparing addresss of nodes in both lists, first matching node is the merge point
 
 
@@ -30,11 +31,82 @@ Node *FindMergePoint(Node *headA,Node *headB) {
 	int m = length(headA);
 	int n = length(headB);
 	
+	struct Node *temp = headB;
+	//trversing lists
+	
+	for(int i=0; i < m ;i++) {
+		
+		//resetting headB to starting node
+		headB = temp;
+		
+		for(int j=0; j < n; j++) {
+			//comparing address
+			if(headA==headB) {
+				//the address or pointer to the merge point
+				cout<<headA->data<<endl;
+				return headA;
+			}
+			
+			headB=headB->next;
+			
+		}
+		headA= headA->next;
+	}
+	return NULL;
+}
+//Inefficient-TIME CPMPLEXITY - O(mn) and high space complexity due to use of pointers
+
+
+
+int main() {
+	
+	//List A
+	struct Node *n1 = new Node();
+	struct Node *n2 = new Node();
+	struct Node *n3 = new Node();
+	struct Node *n4 = new Node();
+	
+	n1->data = 1;
+	n2->data = 3;
+	n3->data = 4;
+	n4->data = 6;
+	
+	//creating links
+	
+	n1->next = n2;
+	n3->next = n4;
+	n2->next = n3;
+	n4->next = NULL;
+	
+	//LIST-A
+	//1(n1) --> 3(n2) --> 4(n3) --> 6(n4) -->NULL
+	
+	//List B
+	struct Node *m1 = new Node();
+	struct Node *m2 = new Node();
+	struct Node *m3 = new Node();
+	
+	
+	m1->data = 2;
+	m2->data = 5;
+	m3->data = 4;
+
+	//LIST-B
+	//2(m1) --> 5(m2) --> 4(n3) --> NULL
+	
+	//creating links
+	m1->next = m2;
+	m2->next = n3;
+	n3->next = NULL;
+	
+	
+	cout<<FindMergePoint(n1,m1);
+	//in this case the Merge point is node n3 which is common in both
+
+	
 	
 	
 	
 }
-
-
 
 
