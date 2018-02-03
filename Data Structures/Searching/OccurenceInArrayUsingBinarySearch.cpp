5f9e2e56b44bab918e355fa4f6b670fbee8a70e6
anishsingh@20 <loveeanish@gmail.com>
2018-02-04 00:31:43 +0530

    added a function to merge sort the array

--- Data Structures/Searching/OccurenceInArrayUsingBinarySearch.cpp
+++ Data Structures/Searching/OccurenceInArrayUsingBinarySearch.cpp
@@ -4,6 +4,93 @@
 
 using namespace std;
 
+
+//Merge sort to sort the given array
+
+void Merge(int arr[],int start,int mid , int end)
+{
+	int i,j,k;
+	int nl,nr;
+	
+	//number of elements in L and R arrays
+	nl = (mid-start) + 1;
+	nr = (end-mid);
+	
+	mid = (start + (end -start)/2 );
+	//new arrays to store the left and right subarrays
+	int l[nl],r[nr];
+	
+	
+	//copying items to the subarrays from the orig array
+	for(i = 0 ; i < nl ; i++) {
+		l[i] = arr[start+i];
+	}
+	
+	for(j= 0 ; j< nr ; j++) {
+		
+		r[j] = arr[mid + 1 + j];
+	}
+	
+	i=0,j=0,k=start;
+	
+	
+	while( i < nl && j < nr ) {
+		
+		if(l[i] < r[j]) 
+		{
+			
+			arr[k] = l[i];
+			i++;
+			
+			
+		}
+		
+		else {
+			
+			arr[k] = r[j];
+			j++;
+			
+		}
+		k++;
+		
+	}
+	
+	//now copying the left overs in new sorted array
+	while( i < nl ) 
+	{
+		arr[k] = l[i];
+		i++;
+		k++;
+	}
+
+	while( j < nr ) 
+	{
+		arr[k] = r[j];
+		j++;
+		k++;
+	}	
+	
+}
+
+void MergeSort(int arr[],int start,int end) {
+	
+	
+	
+	if(start < end) {
+		
+		int mid = (start + (end - start)/2 ) ;
+		
+		MergeSort(arr,start,mid);
+		
+		MergeSort(arr,mid+1,end);
+		
+		Merge(arr,start,mid,end);
+	}
+}
+
+
+
+
 //Binary search can also be used to find first and last occurences of a repeating number in an array
 
 int BinarySearch(int arr[],int n , int x , bool searchFirst)
@@ -45,11 +132,22 @@ int BinarySearch(int arr[],int n , int x , bool searchFirst)
 
 int main () {
 	
-	int arr [] = {1,2,2,2,3,3,6,7,9};
+	int arr [] = {15,10,2,9,2,8,15,2,3};
 	
+	
+	//sorting the array to apply binary search
+	
+	MergeSort(arr, 0 , sizeof(arr)/sizeof(arr[0])-1 );
+	
+	cout<<"Sorted array is:"<<endl;
+	
+	for(int i = 0 ; i < sizeof(arr)/sizeof(arr[0]);i++) {
+		
+		cout<<arr[i]<<endl;
+	}
 	
 	//finding the index of the first occurecne of searched number
-	int firstOcc = BinarySearch(arr,sizeof(arr)/sizeof(arr[0]), 2 , true);
+	int firstOcc = BinarySearch(arr,sizeof(arr)/sizeof(arr[0]), 15 , true);
 	
 	if(firstOcc==-1) {
 		
@@ -58,7 +156,7 @@ int main () {
 	
 	else {
 		//findind the index of the last occurence of the searched number
-		int lastOcc = BinarySearch(arr,sizeof(arr)/sizeof(arr[0]), 2 , false);
+		int lastOcc = BinarySearch(arr,sizeof(arr)/sizeof(arr[0]), 15 , false);
 		
 		cout<<"Count of occurence is :"<<(lastOcc-firstOcc +1)<<endl;
 	}
