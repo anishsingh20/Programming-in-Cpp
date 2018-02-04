1b9df4105901b79e2f6dfbcd8c3b3eec4bede248
anishsingh@20 <loveeanish@gmail.com>
2018-02-04 12:16:40 +0530

    added explantion of templates

--- STL/templateFunc.cpp
+++ STL/templateFunc.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 
+/*
+Templates are expended at compiler time. This is like macros. The difference is, compiler does type checking before template expansion. 
+The idea is simple, source code contains only function/class, but compiled code may contain multiple copies of same function/class.
+*/
+
+
 template<typename T>
 
 void Swap(T *a, T *b) {
