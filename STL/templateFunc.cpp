3d948da6feeed55baa72865c7c409bebee669658
anishsingh@20 <loveeanish@gmail.com>
2018-02-04 22:37:35 +0530

    more examples of using template in cpp

--- STL/templateFunc.cpp
+++ STL/templateFunc.cpp
@@ -11,30 +11,45 @@ The idea is simple, source code contains only function/class, but compiled code 
 
 template<typename T>
 
-void Swap(T *a, T *b) {
+T Swap(T *a, T *b) {
 	
 	T temp = *a;
 	*a=*b;
 	*b=temp;
+}
+
+template<class T>
+
+T max(T a, T b , T c) {
 	
+	if(a > b && a > c) return a;
 	
+	if(b>a && b>c) return b;
+	
+	if(c>a && c>b) return c; 
 }
 
 
 int main () 
 {
-	int a=10,b=20;
-	string a1 = "Anish", b1 = "mrinal";
+//	int a=10,b=20;
+//	string a1 = "Anish", b1 = "mrinal";
+//	
+//	//call by reference
+//	Swap(&a,&b);
+//	
+//	cout<<"a :"<<a<<"  |  "<<"b :"<<b<<endl;
+//	
+//	Swap(&a1,&b1);
+//	
+//	
+//	cout<<a1<<endl;
+//	cout<<b1<<endl;
+
+	cout<<max<int>(10,12,100);
+	cout<<endl;	
+	cout<<max<char>('a','b','z');
 	
-	Swap(&a,&b);
-	
-	cout<<"a :"<<a<<"  |  "<<"b :"<<b<<endl;
-	
-	Swap(&a1,&b1);
-	
-	
-	cout<<a1<<endl;
-	cout<<b1<<endl;
 
 	
 	return 0;
