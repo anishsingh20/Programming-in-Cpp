4b1a1f0ac1d41a15782ffb33e31dca25121e0634
anishsingh@20 <loveeanish@gmail.com>
2018-02-05 11:29:34 +0530

    implemented MergeSort for linked lsists

--- Data Structures/SORTING/MergeSortLinkedList.cpp
+++ Data Structures/SORTING/MergeSortLinkedList.cpp
@@ -18,10 +18,11 @@ int length(struct Node *head) {
 	}
 	return count;
 	
-}
+}//TIME COMPLEXITY   = O(n)
 
 void SplitList(struct Node *head, Node **headA, Node **headB)
 {
+	struct Node *temp=head;
 	if(head==NULL || head->next == NULL)
 	{
 		(*headA) = head;
@@ -30,13 +31,13 @@ void SplitList(struct Node *head, Node **headA, Node **headB)
 	}
 	
 	(*headA) = head;
-	int len = length(head);
+	int len = length(head); //O(n)
 	
 	int l=0,h=(len-1),mid;
 	
 	//finding mid index of list
 	mid = (l + (h-l)/2);
-	struct Node *temp;
+	
 	
 	for(int i=0;i<mid;i++) 
 	{
@@ -49,7 +50,7 @@ void SplitList(struct Node *head, Node **headA, Node **headB)
 	temp->next = NULL;
 	
 	
-}
+}//TIME COMPLEXITY = O(n) and space consumed by the pointers/references
 
 
 //function to merge 2 linked lists in sorted manner
@@ -70,25 +71,80 @@ Node* SortedMerge(struct Node *headA, struct Node *headB)
 		
 		new_head = headB;
 		
-		new_head->next = SortedMerge(headA=B->next,headA );
+		new_head->next = SortedMerge(headA,headB->next);
 	}
 	
 	return new_head;
 	
-	
-	
-}
+} //TIME COMPLEXITY = O(1) , SPACE COMPLEXITY = O(n) due to recursion and usage of stack space
 
-
-
-
-void mergeSort(struct Node *head) 
+//recursive merge sort function
+void mergeSort(struct Node **act_head) 
 {
+	struct Node *head=(*act_head);
+	struct Node *a, *b;
+	//base condition
+	if( head==NULL || head->next == NULL) {
+		return;
+	}
 	
+	//now split the list in 2 sublists
+	SplitList( head , &a , &b );
 	
+	//recursively sorting the sublists
+	mergeSort(&a);
+	mergeSort(&b);
+	
+	//merging the 2 sublists into one sorted list which returns the new head of sorted list
+	*act_head = SortedMerge(a,b);
+
+}//TIME COMPLEXITY = O(nlogn) in worst case, but it is not in-place sorting algorithm, due to 2 new sublists ar formed which consume extra memory 
+
+
+
+//function to traverse the list
+void printList(struct Node **head) 
+{
+	while( (*head)!=NULL ) {
+		
+		cout<<(*head)->data<<"-->";
+		(*head)=(*head)->next;
+	}
+	cout<<"NULL";
 }
+
 
 int main()
 {
+	
+	struct Node *n1 = new Node();
+	struct Node *n2 = new Node();
+	struct Node *n3 = new Node();
+	struct Node *n4 = new Node();
+	struct Node *n5 = new Node();
+	
+	
+	
+	n1->data = 100;
+	n2->data = 2;
+	n3->data = 1;
+	n4->data = 12;
+	n5->data = 10;
+	
+	n1->next = n2;
+	n2->next = n3;
+	n3->next = n4;
+	n4->next = n5;
+	n5->next = NULL;
+	
+	
+	//sorting the list
+	mergeSort(&n1);
+	
+	
+	//printint the sorted list
+	printList(&n1);
+	
+	
 	
 }
