a62600600555810cc7da13b9c632ddb444049e39
anishsingh@20 <loveeanish@gmail.com>
2018-02-05 10:54:09 +0530

    completed Merge and Splitting list functions

--- Data Structures/SORTING/MergeSortLinkedList.cpp
+++ Data Structures/SORTING/MergeSortLinkedList.cpp
@@ -9,14 +9,72 @@ struct Node{
 	int data;
 };
 
-void SplitList(struct Node *head, Node *headA, Node *headB)
+
+int length(struct Node *head) {
+	int count = 0;
+	while(head!=NULL) {
+		count++;
+		head=head->next;
+	}
+	return count;
+	
+}
+
+void SplitList(struct Node *head, Node **headA, Node **headB)
 {
+	if(head==NULL || head->next == NULL)
+	{
+		(*headA) = head;
+		 headB = NULL;	
+		 return;	
+	}
+	
+	(*headA) = head;
+	int len = length(head);
+	
+	int l=0,h=(len-1),mid;
+	
+	//finding mid index of list
+	mid = (l + (h-l)/2);
+	struct Node *temp;
+	
+	for(int i=0;i<mid;i++) 
+	{
+		
+		temp=temp->next;
+	}
+	
+	(*headB) = temp->next;
+	
+	temp->next = NULL;
+	
 	
 }
 
 
 //function to merge 2 linked lists in sorted manner
-Node* SortedMerge(struct Node *headA, struct Node *headB) {
+Node* SortedMerge(struct Node *headA, struct Node *headB) 
+{
+	struct Node *new_head=NULL;
+	//base condition
+	if(headA==NULL) return headB;
+	if(headB==NULL) return headA;
+	
+	if(headA->data <= headB->data) {
+		new_head = headA;
+		
+		new_head->next = SortedMerge(headA->next,headB );
+		
+	}
+	else {
+		
+		new_head = headB;
+		
+		new_head->next = SortedMerge(headA=B->next,headA );
+	}
+	
+	return new_head;
+	
 	
 	
 }
