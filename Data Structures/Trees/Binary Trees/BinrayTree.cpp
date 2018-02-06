fa4113a39f5a55c2d2aa62fd8c9add96c40d78ba
anishsingh@20 <loveeanish@gmail.com>
2018-02-07 02:07:52 +0530

    described the various traversal techniques

--- Data Structures/Trees/Binary Trees/BinrayTree.cpp
+++ Data Structures/Trees/Binary Trees/BinrayTree.cpp
@@ -12,6 +12,16 @@ struct BinaryTreeNode{
 };
 
      
+/*TRAVERSING A BT-
+1)PREORDER - a) visit the root node b)traverse the left subtree in preorder c) visit the right subtree in preorder
+
+2)INORDER - a) Traverse to the left subtree in Inorder  b) visit the root node c) traverse to the right subtree in inorder
+
+3)POSTORDER-a) Traverse to the left subtree in postorder  b) traverse to the right subtree in postorder c) visit the root node 
+
+4) Level order traversal - BFS -breadth first search
+*/   
+     
      
 int main()
 {
