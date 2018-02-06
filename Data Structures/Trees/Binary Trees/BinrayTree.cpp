1212949a0d9be7fb09da939ebac7ff4f63aa337f
anishsingh@20 <loveeanish@gmail.com>
2018-02-07 02:22:24 +0530

    implemented recursive implementation of inorder,postorder and preorder traversal

--- Data Structures/Trees/Binary Trees/BinrayTree.cpp
+++ Data Structures/Trees/Binary Trees/BinrayTree.cpp
@@ -29,9 +29,9 @@ void PreOrder(struct BinaryTreeNode *root)
 {
 	if(root)
 	{
-		cout<<root->data<<"->";
+		cout<<root->data<<" ";
 		PreOrder(root->left);
-		PreORder(root->right);
+		PreOrder(root->right);
 	}
 	
 	
@@ -42,7 +42,7 @@ void InOrder(struct BinaryTreeNode *root)
 	if(root)
 	{
 		InOrder(root->left);
-		cout<<root->data<<"->";
+		cout<<root->data<<" ";
 		InOrder(root->right);
 	}	
 }
@@ -53,7 +53,7 @@ void PostOrder(struct BinaryTreeNode *root)
 	{
 		PostOrder(root->left);
 		PostOrder(root->right);
-		cout<<root->data<<"->";
+		cout<<root->data<<" ";
 	}
 	
 }
@@ -61,6 +61,50 @@ void PostOrder(struct BinaryTreeNode *root)
      
 int main()
 {
+	
+	//generating a full binary tree -  n(no of nodes) = 2^(h+1) -1 , h = height of tree
+	struct BinaryTreeNode *root = new BinaryTreeNode();
+	struct BinaryTreeNode *r1 = new BinaryTreeNode();
+	struct BinaryTreeNode *r2 = new BinaryTreeNode();
+	struct BinaryTreeNode *r3 = new BinaryTreeNode();
+	struct BinaryTreeNode *r4 = new BinaryTreeNode();
+	struct BinaryTreeNode *r5 = new BinaryTreeNode();
+	struct BinaryTreeNode *r6 = new BinaryTreeNode();	
+	
+	root->data = 1;
+	r1->data = 2;
+	r2->data = 3;
+	r3->data = 4;
+	r4->data = 5;
+	r5->data = 6;
+	r6->data = 7;
+	
+	root->left  = r1;
+	root->right = r2;
+	
+	r1->left = r3;
+	r1->right = r4;
+	
+	r2->left = r5;
+	r2->right = r6;
+	
+	r5->left  = r6->left  = NULL;
+	r5->right  = r6->right  = NULL;	
+	
+	
+	cout<<"Pre order traversal outputs-"<<endl;
+	PreOrder(root);
+	
+	cout<<endl;
+	
+	cout<<"In order traversal outputs-"<<endl;
+	InOrder(root);
+	
+	cout<<endl;
+	
+	cout<<"Post order traversal outputs-"<<endl;
+	PostOrder(root);
+
 	
 	return 0;
 }
