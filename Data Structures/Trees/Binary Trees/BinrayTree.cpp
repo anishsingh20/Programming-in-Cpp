f93f66b460daab58464aac34a160020860006e12
anishsingh@20 <loveeanish@gmail.com>
2018-02-07 02:04:50 +0530

    implementation of Binary tree in cpp

---
+++ Data Structures/Trees/Binary Trees/BinrayTree.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+//IMPLEMENTATION OF A BINARY TREE- a tree which can have atmost i.e <= 2 child nodes
+
+using namespace std;
+
+
+struct BinaryTreeNode{
+	int data;
+	BinaryTreeNode *left;
+	BinaryTreeNode *right;
+};
+
+     
+     
+int main()
+{
+	
+	return 0;
+}
+
+
