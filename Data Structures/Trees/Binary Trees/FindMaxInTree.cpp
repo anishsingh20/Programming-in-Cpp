70190a297b34d26200e2df895bfa6211ef985414
anishsingh@20 <loveeanish@gmail.com>
2018-02-08 02:13:37 +0530

    program to find maximum element in a tree

---
+++ Data Structures/Trees/Binary Trees/FindMaxInTree.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+
+/*PROGRAM TO FIND MAXIMUM ELEMENT IN A BINARY TREE-
+1) Recursive implementation-find max in left subtree, find max in right subtree, compare them with the root_node val and return the max amongst them
+2) Non-recursive -using level order traversal 
+*/
+using namespace std;
+
+
+int main()
+
+{
+	return 0;
+}
