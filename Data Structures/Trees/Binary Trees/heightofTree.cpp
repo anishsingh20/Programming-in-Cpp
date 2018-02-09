baa6a1c9e14f1d8d6451fcac9cad5237d4b599ed
anishsingh@20 <loveeanish@gmail.com>
2018-02-10 01:23:46 +0530

    non-recursive function to find height of tree using level order traversal

---
+++ Data Structures/Trees/Binary Trees/heightofTree.cpp
@@ -0,0 +1,198 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+
+//IMPLEMENTATION OF FINDING HEIGHT OF BINARY TREE - height is the longest path from the root of the tree to the deepest node(leaf)
+
+using namespace std;
+
+
+struct BinaryTreeNode{
+	int data;
+	BinaryTreeNode *left;
+	BinaryTreeNode *right;
+};
+
+
+//recursive solution -similar to DFS 
+int height(struct BinaryTreeNode *root)
+{
+	int htL=0,htR=0;
+	if(root==NULL) return 0;
+	
+	else
+        {
+            htL = height(root->left);
+            htR = height(root->right);
+            
+            if(htL < htR)
+            {
+            	return (1 + htR);
+	    }
+	    else
+	    {
+	    	return (1 + htL);
+	    }
+	    
+        }
+        
+		
+	
+}
+
+
+//Non recursive solution to find height of a tree using level order traversal-similar ot BFS
+//end of level is identified with NULL
+int heightLevelOrder(struct BinaryTreeNode *root)
+{
+	queue<BinaryTreeNode *> q;
+	BinaryTreeNode *temp;
+	int level=0;
+	
+	if(!root)
+	{
+		return 0;
+	}
+	
+	q.push(root);
+	
+	//end of first level marked by pushing NULL
+	q.push(NULL);
+		while(!q.empty())
+		{
+			root = q.front();
+			q.pop();
+			
+			//completion of current level i.e when it reaches NULL 
+			if(root==NULL)		
+			{
+				if(!q.empty())
+				{
+					q.push(NULL);		
+				}
+				level++;
+				
+			}
+			
+			else {
+				if(root->left) q.push(root->left);
+				if(root->right) q.push(root->right);
+			}
+		}
+		
+		
+		return level;		
+	
+	
+	
+}
+
+
+//function to traverse the tree in level order-BFS
+void levelOrder(struct BinaryTreeNode *root)
+{
+	struct BinaryTreeNode *temp;
+	queue<BinaryTreeNode *>q;
+	
+	if(!root) return;
+	
+	q.push(root);//push the root into queue
+	
+	while(!q.empty())
+	{
+		temp = q.front();
+		q.pop();
+		
+		cout<<temp->data<<" ";
+		
+		//pushing temp's left and right child if any to queue
+		//we will keep (level+1) children in the queue
+		if(temp->left)
+		{
+			q.push(temp->left);
+		}
+		
+		if(temp->right)
+		{
+			q.push(temp->right);
+		}
+	}
+}     //TIME COMPLEXITY = O(n) , SPACE COMPLEXITY = O(n)
+     
+     
+
+
+void Insert( struct BinaryTreeNode **root,int data)
+{
+	struct BinaryTreeNode *new_node = new BinaryTreeNode();
+	struct BinaryTreeNode *temp;
+	queue<BinaryTreeNode *> q;
+	new_node->data = data;
+	new_node->left=NULL;
+	new_node->right=NULL;
+	
+	if(*root==NULL)
+	{
+		*root = new_node;
+		return;
+	}
+	
+	q.push(*root);
+	
+	while(!q.empty())
+	{
+		temp  = q.front();
+		q.pop();
+		
+		if(temp->left==NULL)
+		{
+			temp->left = new_node;
+			return;
+			
+			
+		}
+		
+		else
+		{
+			q.push(temp->left);
+		}
+			
+		if(temp->right==NULL)
+		{
+			temp->right = new_node;
+			return;
+			
+		}	
+		
+		else
+		{
+			q.push(temp->right);
+		}
+	}
+	
+	
+	
+}
+
+
+
+int main()
+{
+	
+	struct BinaryTreeNode *root=NULL;
+	Insert(&root,1);
+	Insert(&root,2);
+	Insert(&root,3);
+	Insert(&root,4);
+	Insert(&root,5);
+	Insert(&root,10);
+	Insert(&root,11);
+	Insert(&root,12);
+	
+	levelOrder(root);
+	cout<<endl;
+	
+	cout<<heightLevelOrder(root);
+	
+	return 0;
+}
