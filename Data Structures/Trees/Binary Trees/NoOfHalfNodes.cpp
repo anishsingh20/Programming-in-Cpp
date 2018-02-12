e615c8d437c8cc2fafe1d882d38d5b1de7d7fee0
anishsingh@20 <loveeanish@gmail.com>
2018-02-13 00:12:59 +0530

    algorithm to find number of half nodes

---
+++ Data Structures/Trees/Binary Trees/NoOfHalfNodes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+
+//Program to find number of half nodes in a BinaryTRee-half nodes are nodes having only one subchild - either left or right
+using namespace std;
+
+int main()
+{
+	return 0;
+}
