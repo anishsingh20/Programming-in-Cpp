5597e0702d082cbc5e97635791d93c9f1377ab04
anishsingh@20 <loveeanish@gmail.com>
2018-02-14 01:02:05 +0530

    usage of precision,fill and width methods"

---
+++ C++ iostreams/precisionAndFillwidthMethods.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+/*Program to demonstarte the use of precision(),width(),fill() methods belonging to ios class
+1) precision() method can be used to print/display only particular sequence on a floating point number, cannot work on normal integers
+*/
+
+int main()
+{
+	cout.precision(4); //precision
+	cout<<122.112<<endl;//outputs-122.1
+	
+	//width method - to fill the empty width of a character, integer or number
+	
+	cout.width(20);
+	cout.fill('*');
+	
+	cout.setf(ios::left);
+	
+	//for output to be left justified - we can use left flag
+	cout<<"Anish"<<endl;
+	
+	return 0; 
+}
