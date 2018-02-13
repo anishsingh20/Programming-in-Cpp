e690cc52636022d9f04850d81552c8a48cfd9bf1
anishsingh@20 <loveeanish@gmail.com>
2018-02-14 02:07:11 +0530

    program to demonstarte building custom manipulator functions

---
+++ C++ iostreams/makingCustomManipulatorFunctions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+
+/*program to demonstrate how to build custom manipulator functions
+a) these functions can be of 2 types -  one with a parameter like setw(),setprecision() etc
+b) other manipulator methods can be without any parameter like hex, dec , endl
+
+*/
+using namespace std;
+
+int main()
+{
+	return 0;
+}
