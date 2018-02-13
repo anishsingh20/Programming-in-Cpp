ade148ecf5a0c9188221587f7c2a2c7435b0df80
anishsingh@20 <loveeanish@gmail.com>
2018-02-14 02:18:25 +0530

    made manipulator function to manipulate and work with input and output streams

--- C++ iostreams/makingCustomManipulatorFunctions.cpp
+++ C++ iostreams/makingCustomManipulatorFunctions.cpp
@@ -5,10 +5,38 @@
 a) these functions can be of 2 types -  one with a parameter like setw(),setprecision() etc
 b) other manipulator methods can be without any parameter like hex, dec , endl
 
+NOTE-
+Manipulator functions should return either reference to ostream class object os istream class and should take a default argument which is passed by reference of type
+ostream or istream class like demonstrated below
 */
 using namespace std;
 
+
+ostream& print(ostream &output)
+{
+	output<<"Hex  value is:" << hex ;
+//	output<<"Octal value is: "<< oct<<endl;
+//	output<<"decimal value is: "<<dec<<endl;
+	
+	return output;
+}
+
+istream& input(istream &input)
+{
+	
+	
+	cout<<"Enter the data:"<<endl;
+	
+	return input;
+}
+
+
 int main()
 {
+	int x;
+	cin>>input>>x;
+	
+	cout<<print<<x;
+	
 	return 0;
 }
