6dab2d5e56e31af2bc02ac4f1a41b3874f469ff5
anishsingh@20 <loveeanish@gmail.com>
2018-02-14 02:03:22 +0530

    demonstrated use of simple IO manipulators

--- C++ iostreams/IOmanipulators.cpp
+++ C++ iostreams/IOmanipulators.cpp
@@ -5,10 +5,13 @@
 using namespace std;
 
 /*Program to demonstrate various manipulators of input and output streams
+Most general use of manipulator to manipulate or modify output stream is endl 
+Other examples include hex,oct,dec, setw(),setfill() , setprecision() etc
 */
 
 int main()
 {
 	
+	cout<< setw(10) << setfill('#') << setprecision(4) << oct << 100.3344;
 	return 0;
 }
