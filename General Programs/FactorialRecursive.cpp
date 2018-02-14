e75954dfaa180ff667128bee95bc5fea4018a3c8
anishsingh@20 <loveeanish@gmail.com>
2018-02-14 23:15:55 +0530

    recursive program to find factorial of a number

---
+++ General Programs/FactorialRecursive.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+
+using namespace std;
+
+//Recrsive program to find Factorial of a number
+int factorial(int n)
+{
+	if(n==0) return 0;
+	
+	
+	if(n==1) return 1;
+	
+	//otherwise, recursively find factorial of n* factorial(n-1)
+	else
+		return n*factorial(n-1);
+		
+	
+	
+}
+
+
+int main()
+{
+	
+	cout<<factorial(6)<<endl;
+	
+	return 0;
+}
