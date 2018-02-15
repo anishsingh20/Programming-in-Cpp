f5d3b4867b40e9b1eb816f95cbac909a61e8f3f1
anishsingh@20 <loveeanish@gmail.com>
2018-02-16 00:13:36 +0530

    added some descriptiop about recursion and method 2

--- Dynamic Programming/Exponentiation.cpp
+++ Dynamic Programming/Exponentiation.cpp
@@ -2,18 +2,39 @@
 
 //Program to demonstrate Recursive approaches to fidn pow(x,n) with different time complexities
 //First to solve any problem by recursion we need to define and calculate a recurrence relation or recursion defination
+//Space complexity of a recursive program is direclty proportional to the height/depth of the recursion tree
 
 
-int Pow(int x,int n)
+//Method-1
+long long int Pow(int x,int n)
 {
+	//base condition-state where recursion ends and return 1-leaf of recursion tree
 	if(n==0)
 		return 1;
 		
 	return x*Pow(x,n-1);
-} //Time complexity  = O(n) = linear
+} //Time complexity  = O(n) = linear , but very high SPACE COMPLEXITY
 
-int Pow1(int x,int n)
+
+//Method-2 , more efficient, LESSER SPACE COMPLEXITY due to lesser funcition calls states stored in stack 
+long long int Pow1(int x,int n)
 {
+	//base condition-state where recursion ends and return 1-leaf of recursion tree
+	if(n==0)
+		return 1;
+	
+	//if n is even power
+	if(n%2==0)
+	{
+		long long int y = Pow1(x,n/2);
+		return y*y;
+	}
+	
+	//if x is odd
+	else
+	{
+		return x*Pow1(x,n-1);
+	}
 	
 }
 
@@ -22,7 +43,9 @@ using namespace std;
 int main()
 {
 	
-	cout<<Pow(2,10);
-	
+	cout<<Pow(13,60); // takes 61 steps = n  , height of recursion tree is 61,very high space complexity
+	cout<<endl;
+	cout<<Pow1(10,60); //takes  = logN ,around 10 steps , height of recursion tree is 10 , veru low space complexity
+ 	
 	return 0;
 }
