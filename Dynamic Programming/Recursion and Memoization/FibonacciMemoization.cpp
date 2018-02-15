e3c49f4739d1f45c2fd104f39588534aed8b72e7
anishsingh@20 <loveeanish@gmail.com>
2018-02-15 12:43:06 +0530

    first program on DP and finding fibonacci sequence using memoization

--- Dynamic Programming/Recursion and Memoization/FibonacciMemoization.cpp
+++ Dynamic Programming/Recursion and Memoization/FibonacciMemoization.cpp
@@ -1,10 +1,46 @@
 #include<iostream>
 
-//PROGRAM TO USE RECURSION WITH MEMOIZATION TO IMPROVE TIME COMPLEXITY OF RECURSIVE FINONACCI SEQUENCE PROGRAM
+/*PROGRAM TO USE RECURSION WITH MEMOIZATION TO IMPROVE TIME COMPLEXITY OF RECURSIVE FINONACCI SEQUENCE PROGRAM-
+In order to improve the time complexity of which are inefficient when using recursion  and for higher values of n, we can use DP and memoization technique to
+store the results of the subproblems and state of recursive calls in some data structure (say array) and simly return them instead of
+redundantly calculating the same function calls.
+*/
+
+long long int F[51];
+
+long long int Fibonacci(int n)
+{
+	//if result is stored in memory simply return it
+	if(F[n] != -1)
+	{
+		return F[n];
+	}
+		
+	//otherwise simply calculate the result and store it in memory
+	
+	F[n] = ( Fibonacci(n-1) + Fibonacci(n-2) 	);
+	
+	return F[n];
+	
+	
+}
+
 
 using namespace std;
 
 int main()
 {
+	
+	for(int i=0;i<51;i++)
+	{
+		F[i] = -1;
+	}
+	
+	F[0] = 0 , F[1]=1;
+	
+	long long int result  = Fibonacci(50);
+	
+	cout<<result;
+	
 	return 0;
 }
