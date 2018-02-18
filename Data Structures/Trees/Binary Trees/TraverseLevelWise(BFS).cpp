b2542cbc2e96dd7ae5d602a082594b5a9087aa2e
anishsingh@20 <loveeanish@gmail.com>
2018-02-19 00:50:26 +0530

    level order traversal in any order left to right or vice-versa

--- Data Structures/Trees/Binary Trees/TraverseLevelWise(BFS).cpp
+++ Data Structures/Trees/Binary Trees/TraverseLevelWise(BFS).cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+#include<queue>
 
 
 using namespace std;
 
 
+//Recursive implementation to travese a tree in level Order
 struct BinaryTreeNode{
 	int data;
 	BinaryTreeNode *left,*parent;
@@ -12,6 +14,7 @@ struct BinaryTreeNode{
 
 
 //function to traverse a tree level wise in any order form left to right or right to left- Breadth first traversal
+//ltr is the order of traversing, whether to traverse from left to right or from right to left subtree
 void printTree(struct BinaryTreeNode *root,int level, bool ltr)
 {
 	//base condition
@@ -41,3 +44,83 @@ void printTree(struct BinaryTreeNode *root,int level, bool ltr)
 	}
 }
 
+
+
+void Insert( struct BinaryTreeNode **root,int data)
+{
+	struct BinaryTreeNode *new_node = new BinaryTreeNode();
+	struct BinaryTreeNode *temp;
+	queue<BinaryTreeNode *> q;
+	new_node->data = data;
+	new_node->left=NULL;
+	new_node->right=NULL;
+	
+	if(*root==NULL)
+	{
+		*root = new_node;
+		return;
+	}
+	
+	q.push(*root);
+	
+	while(!q.empty())
+	{
+		temp  = q.front();
+		q.pop();
+		
+		if(temp->left==NULL)
+		{
+			temp->left = new_node;
+			return;
+			
+			
+		}
+		
+		else
+		{
+			q.push(temp->left);
+		}
+			
+		if(temp->right==NULL)
+		{
+			temp->right = new_node;
+			return;
+			
+		}	
+		
+		else
+		{
+			q.push(temp->right);
+		}
+	}
+	
+}
+
+
+
+//function the get the height of tree
+int height(BinaryTreeNode *root)
+{
+	if(root==NULL) return 0;
+	
+	else return (1 + max(height(root->left),height(root->right)));
+}
+
+
+int main()
+{
+	struct BinaryTreeNode *root1 = NULL;
+	Insert(&root1,1);
+	Insert(&root1,2);
+	Insert(&root1,3);
+	Insert(&root1,4);
+	Insert(&root1,5);
+	Insert(&root1,6);
+	Insert(&root1,7);
+	
+	for(int i =  0 ; i < height(root1); i++)
+	{
+		printTree(root1,i,false);
+	}
+	
+}
