18029731c220d7d95ba39c4fe7e4e1de83629c99
anishsingh@20 <loveeanish@gmail.com>
2018-02-20 02:46:26 +0530

    appending data to the file and getting put positions

--- File Handling/AppendData.cpp
+++ File Handling/AppendData.cpp
@@ -8,5 +8,23 @@ using namespace std;
 
 int main()
 {
+	fstream file("anish.txt", ios::in | ios::out | ios::app); //setting read, write and append modes of ios class
+	//append will create a new file, if the file being opened does not exists
 	
+	if(!file.is_open())
+	{
+		cout<<"File not found"<<endl;
+	}
+	else
+	{
+		cout<<"Writing data to file"<<endl;
+		
+		//now as many time we re-run the program the below statement is goind to be appended to the file
+		file << "Hi I am Anish."<<endl;
+		cout<<file.tellp();
+		cout<<file.tellg();
+		
+		
+		file.close();
+	}
 }
