d48baa82a8eae118bd27152672af14da5cfb9819
anishsingh@20 <loveeanish@gmail.com>
2018-02-20 01:23:43 +0530

    program to write data to a file

---
+++ File Handling/WritingDatatoFile.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+
+//Program on File handling and opening and closing connections to a file stream
+using namespace std;
+
+//writing data to a file using ostream class object simply like a stream opt
+
+
+int main()
+{
+}
