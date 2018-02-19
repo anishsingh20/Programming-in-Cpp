009208a4de1e5b3c35e4592a066d6ed8930a6e43
anishsingh@20 <loveeanish@gmail.com>
2018-02-20 01:48:39 +0530

    reading all the lines from the file using .good() method

--- File Handling/ReadingData.cpp
+++ File Handling/ReadingData.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 
 using namespace std;
 
@@ -7,5 +8,36 @@ using namespace std;
 
 int main()
 {
+	//creating an object of class ifstream
+	ifstream file;
+	
+	file.open("anish.txt");
+	
+	if(!file.is_open())
+	{
+		cout<<"File not found"<<endl;
+	}
+	
+	else 
+	{
+		
+		//file.good() is a method which checks if flags such as eof bit, bad bit , good bit are set or not
+		//so when file ends and eof flag is set,we come out of loop
+		while(file.good())
+		{
+			//store read data in this variable line
+			string line;
+			//reading entire line using getline()
+			
+			getline(file,line);
+			
+			cout<<line<<endl;	
+		}
+		
+		
+		file.close();
+	}
+	
+	
 	return 0;
 }
