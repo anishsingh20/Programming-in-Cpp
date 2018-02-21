3b8619157e745ef8b31dcf5f1d259d3577cd0b26
anishsingh@20 <loveeanish@gmail.com>
2018-02-22 00:43:28 +0530

    program to store the nodes and their distances from root in a map

---
+++ Data Structures/Trees/STL and Trees/StoreLevelOfEachNodeInMap.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<map>
+#include<queue>
+
+using namespace std;
+
+//Program to store all nodes and their corresponding level/or diatance from root in a HASH MAP
+
+struct BinaryTreeNode{
+	int data;
+	BinaryTreeNode *left,*parent;
+	BinaryTreeNode *right;
+};
+
+void InsertDistInMap(BinaryTreeNode *root,map<BinaryTreeNode*,int>)
+{
+	
+}
+
+
+
+int main()
+{
+	return 0;
+}
