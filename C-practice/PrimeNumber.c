c7119e84714313fe6c1ce1b420bed5aadda3a667
anishsingh@20 <loveeanish@gmail.com>
2018-02-24 03:31:25 +0530

    recursive impleentation of prime number

--- C-practice/PrimeNumber.c
+++ C-practice/PrimeNumber.c
@@ -2,30 +2,47 @@
 #include<conio.h>
 
 
-int Prime(int n)
+int Prime(int n,int i)
 {
-	int flag = 1;
 	
-	if(n==1 || n==0)
-	{
-		return 0;
-	}
 	
-	for(int i=2; i <= n/2 ; i++)
+//	int flag = 1;
+//	
+//	if(n==1 || n==0)
+//	{
+//		return 0;
+//	}
+//	
+//	for(int i=2; i <= n/2 ; i++)
+//	{
+//		if(n%i==0)
+//		{
+//			flag = 0;
+//			
+//		}
+//	}
+//	
+//	return flag;
+
+	if(i<n)
 	{
-		if(n%i==0)
+		if(n%i!=0 )
 		{
-			flag = 0;
-			
+			return Prime(n,++i);
 		}
+		else
+		{
+			return 0;
+		}
+
 	}
 	
-	return flag;
+	return 1;
 }
 
 
 int main()
 {
-	printf("%d",Prime(1));
+	printf("%d",Prime(24,2));
 	return 0;
 }
