eaed95d482365b13095e1343b682ae0289738c4f
anishsingh@20 <loveeanish@gmail.com>
2018-02-25 02:51:46 +0530

    added some more basic pointer questions

--- C-practice/Experimenting.c
+++ C-practice/Experimenting.c
@@ -19,22 +19,26 @@ int main()
 {
 
 
-union u u1;
-struct s s1;
+char c[] = "";
 
-printf("%d",sizeof(u1)); //prints 8 i.e max size is of double in union
-printf("\n"); 
+printf("%d",sizeof(c)); //size is 2 bytes-1 for char and 1 for NUll character
 
-printf("%d",sizeof(s1));//prints 16
-printf("\n"); 
-
-
-int arr[10] = {1,2,3,4,5,6,7};
-
-for(int i=0;i<sizeof(arr)/sizeof(arr[1]);i++)
-{
-	printf("%d ",arr[i]); //prints 0 for the remaining empty values in array
-}
+//union u u1;
+//struct s s1;
+//
+//printf("%d",sizeof(u1)); //prints 8 i.e max size is of double in union
+//printf("\n"); 
+//
+//printf("%d",sizeof(s1));//prints 16
+//printf("\n"); 
+//
+//
+//int arr[10] = {1,2,3,4,5,6,7};
+//
+//for(int i=0;i<sizeof(arr)/sizeof(arr[1]);i++)
+//{
+//	printf("%d ",arr[i]); //prints 0 for the remaining empty values in array
+//}
 
 
 //int *ptr = &(arr+1)[3]; //points to 5 
