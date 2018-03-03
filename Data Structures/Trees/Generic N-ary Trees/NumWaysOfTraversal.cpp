4d91d83f8fb12a2552bed3ccd9abfe2f9d5ef868
anishsingh@20 <loveeanish@gmail.com>
2018-03-03 23:12:38 +0530

    program to find number of ways a N-ary tree can be traversed

---
+++ Data Structures/Trees/Generic N-ary Trees/NumWaysOfTraversal.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+//program to find the number of ways a N-ary tree can be traversed
+
+using namespace std;
+
+//we can use a vector to store the child nodes of a node
+
+struct GenericTree{
+	int data;
+	vector <GenericTree*> child; //a vector to store children of a node
+};
+
+
+
+
+int main()
+{
+	return 0;
+}
