4644b9448e3f438d4c04b4ab82bdaece97174057
anishsingh@20 <loveeanish@gmail.com>
2018-03-04 00:18:06 +0530

    program to print smallest node at each level in a binary tree

---
+++ Data Structures/Trees/Binary Trees/SmallestNodeAtEachLevel.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+
+
+//program to print smallest node at each level of a binary tree
+
+struct BinaryTreeNode{
+	int data;
+	BinaryTreeNode *left;
+	BinaryTreeNode *right;
+};
+
+
+void smallestNode(BinaryTreeNode *root)
+{
+	int small=-1;
+	queue<BinaryTreeNode *>q;
+	BinaryTreeNode* temp;
+}
+
+int main()
+{
+	return 0;
+}
