0f7b556600362b25f9a30b53c5e71d6d0cd84469
anishsingh@20 <loveeanish@gmail.com>
2018-03-03 23:51:02 +0530

    implemented the program to find num of ways to traverse a generic tree

--- Data Structures/Trees/Generic N-ary Trees/NumWaysOfTraversal.cpp
+++ Data Structures/Trees/Generic N-ary Trees/NumWaysOfTraversal.cpp
@@ -20,6 +20,15 @@ Number of ways an N-ary tree can be traversed is the factorial of the number of 
 
 //program to find factorial of a number;
 
+
+GenericTree *NewNode(int data)
+{
+	GenericTree *temp = new GenericTree();
+	temp->data=data;
+	
+	return temp;
+}
+
 unsigned long long int factorial(int n)
 {
 	//base condition
@@ -37,11 +46,11 @@ unsigned long long int factorial(int n)
 
 //function to count total possible ways a N-ary tree can be traversed
 //using level order traversal(BFS) to count the number of child each parent node has at each level
-//usingned long long int inorder to avoid overflow becasue factorials can take very large values
+//usingned long long int inorder to avoid overflow because factorials can take very large values
 unsigned long long int numTraversals(struct GenericTree *root)
 {
 	int ways = 1;
-	if(!root) return;
+	if(!root) return 0;
 	queue<GenericTree *> q;
 	struct GenericTree *temp;
 	
@@ -72,6 +81,51 @@ unsigned long long int numTraversals(struct GenericTree *root)
 
 int main()
 {
+		
+	
+	GenericTree *root = NewNode(1);
+	
+	root->child.push_back(NewNode(2));
+	root->child.push_back(NewNode(3));
+	root->child.push_back(NewNode(4));
+	
+	
+	root->child[0]->child.push_back(NewNode(5));
+	root->child[0]->child.push_back(NewNode(6));
+	root->child[0]->child[1]->child.push_back(NewNode(11));
+	root->child[0]->child[1]->child.push_back(NewNode(12));
+	root->child[0]->child[1]->child.push_back(NewNode(13));
+	
+	
+	root->child[0]->child.push_back(NewNode(7));
+	
+	root->child[1]->child.push_back(NewNode(8));
+	
+	root->child[2]->child.push_back(NewNode(9));
+	root->child[2]->child.push_back(NewNode(10));
+	
+	/* The above tree has structure
+		  1
+		  
+	      /	   |   \
+	      2    3    4  = 3!
+	    / | \  |   / \
+	   5  6  7 8   9  10 = 3! * 1! * 2! 
+	     
+	     / | \		
+	    11 12 13   = 3!
+	
+	TOTAL = 3! * (3!) * 3! * 2! * 1!
+	
+	
+	*/
+	
+	
+	cout<<numTraversals(root);
+	
+	
+	
+
 	
 	return 0;
 }
