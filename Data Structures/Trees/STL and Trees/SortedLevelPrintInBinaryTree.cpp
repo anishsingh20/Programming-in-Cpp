216e00dc30f94cb9f804d0738b0b3466cd57650e
anishsingh@20 <loveeanish@gmail.com>
2018-03-06 13:11:12 +0530

    made the utility function to insert data in map with level as key and all nodes at that level in a vector

--- Data Structures/Trees/STL and Trees/SortedLevelPrintInBinaryTree.cpp
+++ Data Structures/Trees/STL and Trees/SortedLevelPrintInBinaryTree.cpp
@@ -1,6 +1,101 @@
 #include<iostream>
+#include<map>
+
 
 using namespace std;
 
+//PROGRAM TO PRINT NODES AT EACH LEVEL IN SORTED MANNER USING MAPS
 
+
+struct BinaryTreeNode{
+	
+	int data;
+	struct BinaryTreeNode *left,*right;
+};
+
+
+
+void SortedLevelPrintUtil(struct BinaryTreeNode *root, map< int,<vector<int> > &Map,int level)
+{
+	if(!root) return ;
+	
+	SortedLevelPrintUtil(root->left,Map,level+1); //increment level as we recur to each left node
+	
+	Map[level].push_back(root->data); //insert level as key and the node's data in vector
+	
+	SortedLevelPrintUtil(root->right,Map,level+1); //increment level as we recur to each right node
+}
+
+
+
+void Insert( struct BinaryTreeNode **root,int data)
+{
+	struct BinaryTreeNode *new_node = new BinaryTreeNode();
+	struct BinaryTreeNode *temp;
+	queue<BinaryTreeNode *> q;
+	new_node->data = data;
+	new_node->left=NULL;
+	new_node->right=NULL;
+	
+	if(*root==NULL)
+	{
+		*root = new_node;
+		return;
+	}
+	
+	q.push(*root);
+	
+	while(!q.empty())
+	{
+		temp  = q.front();
+		q.pop();
+		
+		if(temp->left==NULL)
+		{
+			temp->left = new_node;
+			return;
+			
+			
+		}
+		
+		else
+		{
+			q.push(temp->left);
+		}
+			
+		if(temp->right==NULL)
+		{
+			temp->right = new_node;
+			return;
+			
+		}	
+		
+		else
+		{
+			q.push(temp->right);
+		}
+	}
+	
+	
+}
+
+
+
+
+int main()
+{
+	struct BinaryTreeNode *root1= NULL;
+	
+	Insert(&root1,1);
+	Insert(&root1,2);
+	Insert(&root1,3);
+	Insert(&root1,4);
+	Insert(&root1,5);
+//	Insert(&root1,2);
+//	Insert(&root1,1);
+
+return 0;
+
+
+}
 
