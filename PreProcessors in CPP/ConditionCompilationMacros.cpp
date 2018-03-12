8a7ec9f41d10eac09220cb6dffba0fd6085dc94b
anishsingh@20 <loveeanish@gmail.com>
2018-03-13 02:50:04 +0530

    conditional compilation macros in cpp

---
+++ PreProcessors in CPP/ConditionCompilationMacros.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+//CONDITIONAL COMPILATION MACROS
+
+#define WIN 1
+#define LIN 2
+#define MAC 3
+
+
+ #define OS MAC
+int main()
+{
+	
+	#if OS == WIN
+		cout<<"Windows OS macro"<<endl;
+	
+	#elif OS == LIN
+		cout<<"LINUX OS macro"<<endl;
+		
+	#else
+		cout<<"MAC OS macro"<<endl;
+		
+	
+	#endif
+	
+	return 0;
+}
