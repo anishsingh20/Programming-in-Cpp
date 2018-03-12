1264b58cb75261556e0b610ce8c6098db0763990
anishsingh@20 <loveeanish@gmail.com>
2018-03-12 11:32:14 +0530

    program to find max sum of hourglass elements in a 2-D array

---
+++ Data Structures/Arrays/HourGlass.cpp
@@ -0,0 +1,65 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main()
+{
+    vector< vector<int> > arr(6,vector<int>(6));
+    for(int arr_i = 0;arr_i < 6;arr_i++){
+       for(int arr_j = 0;arr_j < 6;arr_j++){
+          cin >> arr[arr_i][arr_j];
+       }
+    }
+       
+        
+    //we need to only traverse the first 4 rows for hourglass
+        int maxsum = -1000 , col = 0;
+        for(int i=0;i<4;i++)
+        {
+            int sum = 0; //to store the sums of hourglass elements
+            for(int j = col ; j < col + 3 ; j++ )
+            {
+                sum += arr[i][j];
+                if(j == col )
+                    sum += arr[i+1][col+1]; //add the middle of hourglass
+            
+                sum += arr[i+2][j]; //now add the the third row elements of hourglass
+            }
+            col = (col<3) ? col+1 : 0 ;  
+            if(sum > maxsum)
+            {
+                maxsum = sum;
+            }
+            
+            if(col!=0) i--;
+        }
+        
+        cout<<maxsum;
+        
+    
+   
+    return 0;
+}
+
