8cdbf4132801b36dba046832116c2684555a49c7
anishsingh@20 <loveeanish@gmail.com>
2018-03-13 00:16:54 +0530

    program to read and write to a binary file using read() and write()

--- Data Structures/Arrays/HourGlass.cpp
+++ Data Structures/Arrays/HourGlass.cpp
@@ -1,25 +1,4 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
+#include<iostream>
 
 using namespace std;
 
@@ -59,7 +38,9 @@ int main()
         cout<<maxsum;
         
     
-   
+    int a = 15,b=10;
+    int c= a|b;
+   cout<<c;
     return 0;
 }
 
---
+++ File Handling/ReadAndWriteBinaryFile.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+
+
+//program to read and write to a binary file using read() and write() methods
