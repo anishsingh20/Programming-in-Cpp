6aef87d0efe8c24647ef7fb315114dbd8b94ab6a
anishsingh@20 <loveeanish@gmail.com>
2018-03-13 01:14:40 +0530

    string stream class

---
+++ C++ iostreams/StringStream.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<sstream>
+
+
+using namespace std;
+
+//program to demonstrate the usage of string stream class in CPP to convert any type of stream to a string stream and vice versa 
+
+
