06b5cb7fdf705b438a56eb5d1d5b4f9f4ecf4faa
anishsingh@20 <loveeanish@gmail.com>
2018-03-13 03:04:03 +0530

    demonstrated the use of ifdef,ifndef and undef macros

--- PreProcessors in CPP/ifdefAndifndefMacro.cpp
+++ PreProcessors in CPP/ifdefAndifndefMacro.cpp
@@ -5,3 +5,30 @@
 /* 1) ifdef= if a macro is defined then do something
    2) ifndef = if  macro is not defined then do something	
 */
+
+using namespace std;
+
+#define ANISH 1
+
+int main()
+{
+	#ifdef ANISH
+		cout<<"ANISH macro is defined"<<endl;
+		
+	#else 
+		cout<<"Not defined"<<endl;
+		
+	#endif
+	
+	cout<<endl;
+	
+	#undef ANISH  //undefining the ANISH macro
+	
+	#ifndef ANISH
+		cout<<"ANISH macro undefined"<<endl;
+		
+	#endif
+	
+	
+	return 0;
+}
