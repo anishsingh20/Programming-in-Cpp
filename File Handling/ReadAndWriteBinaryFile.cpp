ab2f1858ad107a3bf1de69534d610695ac9cdeca
anishsingh@20 <loveeanish@gmail.com>
2018-03-13 00:40:40 +0530

    read and written the class object to the file

--- File Handling/ReadAndWriteBinaryFile.cpp
+++ File Handling/ReadAndWriteBinaryFile.cpp
@@ -5,7 +5,7 @@
 
 //program to read and write to a binary file using read() and write() methods
 
-
+using namespace std;
 
 class Person{
 	
@@ -36,6 +36,7 @@ class Person{
 int main()
 {
 	Person Anish("Anish",20);
+	Person Anish2;
 	
 	//creating file object
 	fstream file("AnishBin.bin",ios::binary | ios::in | ios::out | ios::trunc);
@@ -47,6 +48,30 @@ int main()
 	else
 	{
 		//writing a block of data to the binary file
+		//write() function takes argument as data to write and the stream size of the data which is being written
+		file.write(  (char*)&Anish,sizeof(Person) );
+		
+		//now reading data from file
+		//first setting the read position to 0
+		file.seekg(0);
+		
+		
+		while(!file.eof())
+		{
+			//reading data from file and storing it in the object
+			file.read((char*)&Anish2,sizeof(Person) );
+		}
+		
+		Anish.introduce();
+		cout<<endl;
+		Anish2.introduce();
+	
+	
+	
+		file.close();
 		
 	}
+		
+	
+
 }
