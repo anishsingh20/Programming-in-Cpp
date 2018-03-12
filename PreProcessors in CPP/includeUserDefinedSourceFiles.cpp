3fe4940065ff5a3d15d64539e25e3322d9b91362
anishsingh@20 <loveeanish@gmail.com>
2018-03-13 01:51:01 +0530

    loaded a user-defined source file

--- PreProcessors in CPP/includeUserDefinedSourceFiles.cpp
+++ PreProcessors in CPP/includeUserDefinedSourceFiles.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
 
-#include "myClass.cpp"
+#include "MyInfoCLass.cpp"
 
 
-//program to demonstrate the usage of #include pre-processor directive in CPP to include and load custom build source files(classes) 
+/*program to demonstrate the usage of #include pre-processor directive in CPP to include and load custom build source files(classes)
+
+Pre-processor- is simply a software of a small program which run before the compilation of the program
+Pre-processor directive-: using a diretive we can tell a the compiler to compile certain parts of our program based on our instruction
+ */
+ 
+ 
+ 
+int main()
+{
+	Person Anish(20,"Anish");
+	
+	Anish.getInfo();
+	
+	return 0;
+}
