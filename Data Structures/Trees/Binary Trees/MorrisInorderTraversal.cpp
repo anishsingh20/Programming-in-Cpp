d2bdca0b312484078d74896b68c5a05f9477ea07
anishsingh@20 <loveeanish@gmail.com>
2018-03-14 13:14:42 +0530

    added some description

--- Data Structures/Trees/Binary Trees/MorrisInorderTraversal.cpp
+++ Data Structures/Trees/Binary Trees/MorrisInorderTraversal.cpp
@@ -11,7 +11,7 @@ struct Node {
 	
 	int data;
 	Node *left,*right;
-	Node *rightThread;//bool var to indicate that a node has a right pointer point to its inorder succssor
+
 };
 
 Node *newNode(int data){
@@ -19,7 +19,132 @@ Node *newNode(int data){
 	
 	temp->data = data;
 	temp->right=temp->left=NULL;
-	temp->rightThread=NULL;
+
 	
 	return temp;
 }
+
+Node *rightMost(Node *root)
+{
+	if(!root) return NULL;
+	
+	if(root->left==NULL && root->right==NULL)
+		return root;
+		
+	rightMost(root->right);
+}
+
+
+Node* leftMost(Node *root)
+{
+	
+	//recursive solutuion
+	if(!root) return NULL;
+	
+	//when left most leaf is found
+	if(root->left==NULL && root->right==NULL)
+		return root;
+	
+	//otherwise simply recur to the left subtree of each node
+	leftMost(root->left);
+}
+
+
+Node *Predecessor(Node *root)
+{
+	if(!root) return NULL;
+	
+	Node *pre = root->left;
+	
+	
+	
+	while(pre->right!=NULL && pre->right!=root)
+		pre=pre->right;
+	
+
+		
+		
+		
+			
+	return pre;
+	
+	
+}
+
+void MorrisTraversal(Node *root)
+{
+	Node *curr,*pre;
+	
+	if(!root) return ;
+	
+	curr=root;
+	
+	while(curr!=NULL)
+	{
+		//if curr has no left child , then simply visit it
+		if(curr->left==NULL)
+		{
+			cout<<curr->data<<" ";
+			
+			curr=curr->right;
+		}
+		
+		
+		else
+		{
+			//finding the inorder predecessor of current node
+			pre = curr->left;
+			
+		//finding the right most child in the left subtree which is the inorder predecessor of current
+			while(pre->right!=NULL && pre->right!=curr)
+				pre = pre->right;
+
+		/*make current as right child of its inorder predecessor
+			Make current as right child of the rightmost 
+         		node in current's left subtree*/
+				if(pre->right==NULL)
+				{
+					//making link between current and it's inorder succesors right link
+					pre->right = curr;
+					
+					//go to the left child
+					curr= curr->left;
+					
+				}
+				
+				
+				//revert back the changes made in the tree
+				
+				else
+				{
+					pre->right=NULL;
+					
+					cout<<curr->data<<" ";
+					
+					curr = curr->right;
+					
+				}		
+			
+			}
+		
+		}
+	
+}
+
+
+int main()
+{
+	Node *root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->right->left = newNode(7);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->right = newNode(6);
+	
+	MorrisTraversal(root);
+
+
+	
+}
+
