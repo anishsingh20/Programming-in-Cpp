c845588906b5e03db9c50721c0578d2a2a24bce9
anishsingh@20 <loveeanish@gmail.com>
2018-03-16 00:30:38 +0530

    demonstarted how to declare and make a generic function

--- Generic Programming/GenericFunctions.cpp
+++ Generic Programming/GenericFunctions.cpp
@@ -5,3 +5,18 @@ using namespace std;
 
 
 //GENERIC finctions and template keyword to generate a general data type
+
+template<typename T>
+
+void display(T x)
+{
+	cout<<x<<endl;
+}
+
+int main()
+{
+	display(20);
+
+	display("Anish singh walia");
+	display(20.335);	
+}
