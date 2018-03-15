26b4deaf5622a495d1051c6980a542ab930f36e3
anishsingh@20 <loveeanish@gmail.com>
2018-03-15 23:43:50 +0530

    showed how to use the pre-defined preprocessors

--- PreProcessors in CPP/LinePreprocessor.cpp
+++ PreProcessors in CPP/LinePreprocessor.cpp
@@ -7,4 +7,22 @@ using namespace std;
 int main()
 {
 	
+	cout<<"The line number is:"<<__LINE__<<endl;
+	cout<<"THe file is"<<__FILE__<<endl;
+	
+	cout<<"The time of compilation of this program is:"<<__TIME__<<endl;
+	
+	cout<<"The date of source code conversion to object code is:"<<__DATE__<<endl;
+	
+	cout<<"Our compiler can accept standard c and c++ code:"<<__STDC__<<endl;
+	
+	cout<<"Whether our compiler is confirming to the c++ standard:"<<__cplusplus<<endl;
+	
+	
+	//usage if line directive to change line number and file name
+	
+	#line 1200 "anish.txt"
+	
+	cout<<"Now the line is:"<<__LINE__+__LINE__<<endl;
+	cout<<"Now the file is:"<<__FILE__<<endl;
 }
