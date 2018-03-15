a9d25268730eaa469bd0c12c034e30f1c96fc17c
anishsingh@20 <loveeanish@gmail.com>
2018-03-16 00:32:12 +0530

    added some explanation

--- Generic Programming/GenericFunctions.cpp
+++ Generic Programming/GenericFunctions.cpp
@@ -4,8 +4,13 @@
 using namespace std;
 
 
-//GENERIC finctions and template keyword to generate a general data type
+/*GENERIC finctions and template keyword to generate a general data type-
+Generic functions are functions which can operate on any type of data as argument which is passed at compile time.
+And to make a generic data type in cpp we use templates
+*/
 
+
+//making a generic data type T
 template<typename T>
 
 void display(T x)
