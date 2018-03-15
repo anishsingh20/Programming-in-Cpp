6bd275c2f98945f55e998197e872a795f9b3d67f
anishsingh@20 <loveeanish@gmail.com>
2018-03-16 01:35:23 +0530

    demonstrated how to pass default template argumenrs for generic classes

--- Generic Programming/GenericClassDefaultArguments.cpp
+++ Generic Programming/GenericClassDefaultArguments.cpp
@@ -5,9 +5,44 @@ using namespace std;
 
 //program to demonstrate how to pass default template arguments for a generic class
 
+
+template<class t1=int,class t2=string> class Person{
+	
+	t1 age;
+	t2 name;
+	
+	public:
+		Person(t1 age,t2 name)
+		{
+			this->age = age;
+			this->name = name;
+		}
+
+
+	void introduce()
+	{
+		cout<<"The age is: "<<age<<" and name is: "<<name<<endl;
+	}
+
+
+};
+
+ 
 int main()
 {
 	
+	Person <> obj1(20,"anish");
+	s
+	Person <float> obj2(20.0099,"anish singh");
+	
+	Person<string,int> obj3("anish walia",25);
+	
+		
+	obj1.introduce();
+	
+	obj2.introduce();
+	
+	obj3.introduce();
 	
 	return 0;
 }
