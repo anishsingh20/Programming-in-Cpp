83770aace56b8370ca7b3be941c2473bd28e262f
anishsingh@20 <loveeanish@gmail.com>
2018-03-17 01:21:08 +0530

    bottom most view of a binary tree

---
+++ Data Structures/Trees/Binary Trees/BottomView.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<queue>
+
+
+using namespace std;
+
+/*Program to print the bottom view of a binary tree-
+Bottom view of a binary tree is the bottomost node at its horizontal distance. Horizontal distance is -1 for a left edge and +1 for a right edge.
+Whereas Topview was the top most node at its horizontal distance.
+*/
