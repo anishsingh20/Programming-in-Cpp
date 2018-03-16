424d136f2fa4d08a582e88a1d10517c780efec44
anishsingh@20 <loveeanish@gmail.com>
2018-03-17 00:26:44 +0530

    made a function to conver to predecessor threaded tree

--- Data Structures/Trees/Threaded Binary Trees/ConvertBinaryTreeToPredecessorThreadedTree.cpp
+++ Data Structures/Trees/Threaded Binary Trees/ConvertBinaryTreeToPredecessorThreadedTree.cpp
@@ -8,3 +8,64 @@ A predecessor threaded binary tree is a type of threaded tree in which the left 
 This type of threaded tree is used in reverse inorder traversal and postorder traversal too.
 
 */
+
+struct Node {
+	
+	int data;
+	Node *left,*right;
+	bool isThreaded;
+};
+
+
+Node *newNode(int data){
+	Node *temp = new Node;
+	
+	temp->data = data;
+	temp->right=temp->left=NULL;
+	
+	return temp;
+}
+
+//method to conver a binary tree to predecessor threaded tree-using the fact that we link from inorder successors to the current node
+
+Node *ConvertPredecessorThreaded(Node *root)
+{
+	if(!root) return NULL;
+	
+	if(root->left==NULL && root->right==NULL)
+		return root;
+	
+	//now finding the inorder successor of current root-left most in right subtree, or right child itself	
+	if(root->right!=NULL)
+	{
+		Node *r = ConvertPredecessorThreaded(root->right);
+		
+		r->left = root;//making the left link of inorder successor of current point to current node i.e creating a predecessor thread
+		r->isThreaded = true;
+	}
+	
+	//if left is null return root
+	if(root->left==NULL)
+		return root;
+		
+	return ConvertPredecessorThreaded(root->left);
+}
+
+
+int main()
+{
+	
+	Node *root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->right->left = newNode(7);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->right = newNode(6);
+	
+	ConvertPredecessorThreaded(root);
+	
+	
+	
+	
+}
