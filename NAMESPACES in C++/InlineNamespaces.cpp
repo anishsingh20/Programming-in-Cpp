266fdb43e80a11abbd7d3f25b8862830d75043ca
anishsingh@20 <loveeanish@gmail.com>
2018-03-17 13:46:04 +0530

    demonstrated what are inline namespaces

--- NAMESPACES in C++/InlineNamespaces.cpp
+++ NAMESPACES in C++/InlineNamespaces.cpp
@@ -3,11 +3,67 @@
 
 using namespace std;
 
-//inline namesapces
+//inline namespaces- are simply nested namespaces which behave like their parent namespace member
+
+
+namespace AppVersion{
+
+	namespace Version1{
+		
+		void display()
+		{
+			cout<<"This is version 1"<<endl;
+			
+		}
+		
+	}
+	
+	namespace Version2{
+	
+		void display()
+		{
+			cout<<"This is version 2"<<endl;
+			
+		}
+		
+	
+	}
+	
+	
+	inline namespace Version3 {
+		
+		void display()
+		{
+			cout<<"This is the latest update of version three"<<endl;
+		}
+		
+	}
+	
+	//this defination of Version3 namespace is also inline
+	namespace Version3 {
+	
+		void whoAreYou()
+		{
+			cout<<"I am Anish Singh Walia"<<endl;
+		}
+	}
+	
+	
+	
+	
+	
+}
+
+
 
 
 int main()
 {
 	
+	AppVersion::display();
+	AppVersion::whoAreYou();
 	
+	AppVersion::Version1::display();
+
+	AppVersion::Version2::display();	
 }
