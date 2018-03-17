886c9d20ccc8861af8362b6018c0e2c0433926b9
anishsingh@20 <loveeanish@gmail.com>
2018-03-17 12:45:47 +0530

    multiple namespaces can be defined inside a file

--- NAMESPACES in C++/multipleNamespacesInFile.cpp
+++ NAMESPACES in C++/multipleNamespacesInFile.cpp
@@ -4,3 +4,40 @@ using namespace std;
 
 
 //A program/file can have multipe namespaces and each namesapce can be redefined multiple times
+
+namespace a {
+
+	int x;
+	void intro()
+	{
+		cout<<"Hi,! this is namespace a,and value of x  = "<<x<<endl;
+	}
+
+}
+
+
+namespace b
+{
+	int y;
+	void intro()
+	{
+		cout<<"Hi!, this is namespace b, and value of y = "<<y<<endl;
+	}
+}
+
+using namespace a;
+using namespace b;
+
+
+int main()
+{
+	
+	x=10;
+	
+	y=20;
+	
+	a::intro();
+	
+	b::intro();
+	
+}
