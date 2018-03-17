5b430139c85cf5036108eaa66ff61cb9bb43644a
anishsingh@20 <loveeanish@gmail.com>
2018-03-17 13:31:03 +0530

    nesting named namesapce inside an unnamed namespace

--- NAMESPACES in C++/Un-namedNamesapces.cpp
+++ NAMESPACES in C++/Un-namedNamesapces.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 //Un-named or anynymous namespaces. A namespcae which has no name. Each program/file has its own un-named namespace by default, and these unnamed namsapces are loacl
-//to a file and cannot span multiple files.
+//to a file and cannot span multiple files. We can also generate nested unnamed namespaces and also nest unnamed namesapces inside named namesapces or vice versa
 
 
 namespace {
@@ -19,6 +19,19 @@ namespace {
 		else return n*factorial(n-1);	
 	}
 	
+	
+	//nesting named namesapce inside an un-named namesapce
+	namespace b{
+	
+		int x;
+		void fact()
+		{
+			cout<<factorial(x);
+		}
+		
+	
+	}
+	
 }
 
 
@@ -26,6 +39,8 @@ int main()
 {
 	
 	
-	cout<<factorial(30);	
+	b::x=6;
+	b::fact();
+	
 
 }
