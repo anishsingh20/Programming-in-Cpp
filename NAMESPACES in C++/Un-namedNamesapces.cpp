6d8c3c7e432637f34d7c239f7057b392c8f2435a
anishsingh@20 <loveeanish@gmail.com>
2018-03-17 13:26:43 +0530

    demonstration of an unnamed namespace

--- NAMESPACES in C++/Un-namedNamesapces.cpp
+++ NAMESPACES in C++/Un-namedNamesapces.cpp
@@ -3,4 +3,29 @@
 
 using namespace std;
 
+//Un-named or anynymous namespaces. A namespcae which has no name. Each program/file has its own un-named namespace by default, and these unnamed namsapces are loacl
+//to a file and cannot span multiple files.
 
+
+namespace {
+	
+	
+	int factorial(int n)
+	{
+		
+		if(n<=1)
+			return n;
+			
+		else return n*factorial(n-1);	
+	}
+	
+}
+
+
+int main()
+{
+	
+	
+	cout<<factorial(30);	
+
+}
