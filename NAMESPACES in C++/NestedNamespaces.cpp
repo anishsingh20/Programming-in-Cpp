b0f2b0443c25ab1a942fb01cc07a6cb31a7479ce
anishsingh@20 <loveeanish@gmail.com>
2018-03-17 13:00:29 +0530

    example of nesting namespaces

--- NAMESPACES in C++/NestedNamespaces.cpp
+++ NAMESPACES in C++/NestedNamespaces.cpp
@@ -4,3 +4,32 @@ using namespace std;
 
 
 //Nested namespaces- we can nest a namesapce inside another namespace
+
+namespace parent {
+
+	int x ;
+	
+	namespace child{
+	
+		//overriding x
+		int x=100;
+		void display()
+		{
+			cout<<x<<endl;
+		}
+	}
+	
+	void display()
+	{
+		cout<<x;
+	}
+}
+
+int main()
+{
+	parent::x=10;
+	parent::display();
+	cout<<endl;
+	parent::child::display();
+	
+}
