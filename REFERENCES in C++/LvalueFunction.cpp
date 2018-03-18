a9ca2e8f8c61793d17d332e42751dbc48ec66bee
anishsingh@20 <loveeanish@gmail.com>
2018-03-18 12:43:16 +0530

    differece when returning a static var and non-static member in a function which returns a reference

--- REFERENCES in C++/LvalueFunction.cpp
+++ REFERENCES in C++/LvalueFunction.cpp
@@ -12,6 +12,15 @@ int &f()
 	return x;
  } 
  
+ 
+int &fun()
+{
+	int x = 10; //x is loacl variable that means it is not shared amongst function calls and x=10 is executed every time the function is called, stored in memory every time it is 
+	//called
+	
+	return x;
+ } 
+  
 
 int main()
 {
@@ -19,4 +28,10 @@ int main()
 	f()  = 30; // modifies x to 30
 	
 	cout<<f(); //outputs 30	
+	
+	
+	cout<<endl;
+	
+	fun()=20;
+	cout<<fun();//outputs 10 only, no  changes
 }
