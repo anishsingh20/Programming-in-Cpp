3ed2d78f086eb4d7055c69ff22c4a9825057e42e
anishsingh@20 <loveeanish@gmail.com>
2018-03-18 12:32:32 +0530

    demonstrated some facts about references in cpp

--- REFERENCES in C++/FactsAboutReferences.cpp
+++ REFERENCES in C++/FactsAboutReferences.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 /*SOME FACTS ABOUT REFERENCES-
 	1)A reference must be initialized when declared
-	2)Once a reference is crated, it cannot be later made to reference another  object i.e it cannot be reset.
+	2)Once a reference is created, it cannot be later made to reference another  object i.e it cannot be reset.
 	3)Reference cannot be NULL
 	4)References can refer to constant values by making a constant reference to point to a constant variable.
 */
@@ -14,7 +14,15 @@ int main()
 	//1) statement
 	int y=10;
 	int &x=y; //reference variable must be initialized when declared
+
+
+	//3)int &z=NULL; produces error , as a reference variable cannot be NULL
 	
-	cout<<x;
+	//4) Constant references refer to constant var
+	const int b=100;
+	//int & a = b; - produces error 
+	const int &a = b ;
+	cout<<a<<endl;
+		
 	return 0;
 }
