d0f8c85c4268cb057e9358f5eef37a82442c28da
anishsingh@20 <loveeanish@gmail.com>
2018-03-18 23:24:00 +0530

    showed how to delete an object using delete this

--- this Pointer/DeleteThis.cpp
+++ this Pointer/DeleteThis.cpp
@@ -4,11 +4,42 @@ using namespace std;
 
 /*  destructor and this pointer-
 
-'delete this' inside a destructor only works for objects created using new keyword.
+'delete this' inside a destructor or any member function 
+only works for objects created using new keyword.
 
 */
+class Person{
+	
+	int x;
+	
+	public:
+		Person() { 
+			
+			cout<<"Constructor called"<<endl;
+			x = 0;
+		}
+		
+		void destroy()
+		{
+			cout<<"destructor called"<<endl;
+			delete this;
+		}
+		
+	
+};
 
 int main()
 {
+	//works fine
+	Person *a = new Person();
+	Person b;
+	
+	b.destroy();//produces undefined behavior
+	
+	//works fine- as object is allocated using new operator	
+	a->destroy();
+	
+	
+	
 	
 }
