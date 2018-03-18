bf9d9152b3193f475bac246ac86fa0962d9a9620
anishsingh@20 <loveeanish@gmail.com>
2018-03-18 23:09:54 +0530

    destructor and this pointer

---
+++ this Pointer/DeleteThis.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+/*  destructor and this pointer-
+
+'delete this' inside a destructor only works for objects created using new keyword.
+
+*/
+
+int main()
+{
+	
+}
