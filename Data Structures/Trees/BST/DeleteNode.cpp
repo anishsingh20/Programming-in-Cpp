b645cb702520505bb3186572a523c2b8e1aceef3
anishsingh@20 <loveeanish@gmail.com>
2018-03-20 02:38:43 +0530

    program to delete a node in binary search tree

--- Data Structures/Trees/BST/DeleteNode.cpp
+++ Data Structures/Trees/BST/DeleteNode.cpp
@@ -13,6 +13,88 @@ struct BSTnode{
 };
 
 
+
+
+BSTnode *maxNode(BSTnode *root)
+{
+	if(!root) return NULL;
+	
+	
+	if(root->right==NULL)
+		return root;
+		
+	else return maxNode(root->right);
+}
+
+
+BSTnode* Min(BSTnode*root)
+{
+	if(!root)	return NULL;
+	
+	if(root->left==NULL)
+		return root;
+		
+	else return Min(root->left);
+}
+
+//recursive implementation to delete a node
+BSTnode *deleteNode(BSTnode *root,int value)
+{
+	BSTnode *temp;
+	if(!root) return NULL;
+	
+	//first we need to search for the node to delete 
+	
+	if(root->data > value)
+	{
+		root->left  = deleteNode(root->left,value);
+	}
+	
+	else if(root->data < value)
+	{
+		root->right = deleteNode(root->right,value);
+	}
+	
+	else //when node is found
+	{
+		//case1- when the node to be deleted has both left and right children
+		if(root->left && root->right)
+		{
+			//replace the nodes to be deleted data's with its inorder predecessor i.e largest in left subtree
+			//or we can also replace the current root's data with its inorder successor-i.e smallest in right subtree
+			temp = maxNode(root->left);
+			
+			//replacing data
+			root->data  = temp->data;
+			
+			root->left = deleteNode(root->left,value);
+			
+		}
+		
+		//case 2- if the node to be deleted has only 1 child
+		else
+		{
+			temp = root;
+			
+			if(root->left)
+				root= root->left;
+				
+			if(root->right)
+				root = root->right;
+			
+			delete temp;
+		}
+	}
+	
+	return root;
+	
+} //TIME COMPLEXITY = O(n), SPACE COMPLEXITY  = O(n) -recursion call stack
+
+
+
+
+
+
 BSTnode *newNode(int data)
 {
 	BSTnode *temp = new BSTnode();
@@ -78,5 +160,14 @@ int main()
 	insert(root,12);
 	insert(root,2);
 	insert(root,1);
+	
+	Inorder(root);
+	cout<<endl;
+	
+	//deleting node with key 10
+	deleteNode(root,10);
+	
+	Inorder(root);
+	
 	
 }
