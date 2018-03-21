03d5391482e260f384221d224f5362e737c5d88d
anishsingh@20 <loveeanish@gmail.com>
2018-03-22 03:39:41 +0530

    program to find Kth smallest node in BST

---
+++ Data Structures/Trees/BST/KthSmallestInBST.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+//Program to find kth smallest element from a BST- The idea is to do inorder traverasl of BST as we know
+//inorder traversal outputs a sorted list and while traversing keep trach of number of elements visited
+
+
+struct BSTnode{
+	
+	int data;	
+	struct BSTnode *left,*right;
+};
+
+
+void KthSmallestNode(BSTnode *root,int k, int &count)
+{
+	
+	
+	
+	if(root && count <= k)
+	{
+		KthSmallestNode(root->left,k,count);
+		count++; //keeping count of visited items in inorder traversal
+	
+		if( count==k )
+		{
+			cout<<root->data<<endl;
+			
+		}
+	
+
+		KthSmallestNode(root->right,k,count);
+		
+	}
+		
+	
+}
+
+BSTnode *newNode(int data)
+{
+	BSTnode *temp = new BSTnode();
+	
+	temp->data = data;
+	
+	temp->left = temp->right = NULL;
+	
+	return temp;
+}
+
+
+BSTnode *insert(BSTnode *root,int value)
+{
+	//we first need to find the appropiate location to insert data
+	if(root==NULL)
+	{
+		BSTnode *temp  = new BSTnode();
+		temp->data = value;
+		temp->left = temp->right = NULL;
+		
+		root = temp;
+	}
+	
+	
+	else if(root->data > value)
+	{
+		root->left = insert(root->left,value);
+	}
+		
+	else if(root->data < value)
+	{
+		root->right = insert(root->right,value);
+	}
+	
+	
+	
+	//returning root of the updated tree
+	return root;
+}
+
+
+int main()
+{
+	BSTnode *root  = newNode(4);
+	
+	insert(root,2);
+	insert(root,3);
+	insert(root,1);
+	insert(root,7);
+	insert(root,6);
+	insert(root,12);
+	insert(root,20);
+	
+	int c = 0;
+	
+	
+	KthSmallestNode(root,1,c);
+	
+		
+}
