9ab69b6670adf70e4ce86b6100da382ce3cdfa63
anishsingh@20 <loveeanish@gmail.com>
2018-03-22 00:53:44 +0530

    made the util functions to find middle node and length of list

--- Data Structures/Trees/BST/LinkedListToBST.cpp
+++ Data Structures/Trees/BST/LinkedListToBST.cpp
@@ -21,10 +21,44 @@ struct node{
 };
 
 
+//function to count the length of list i.e number of nodes
+int lengthList(struct node *head)
+{
+	int count=0;
+	
+	while(head)
+	{
+		head=head->next;
+		count++;
+	}
+	
+	return count;
+	
+	
+}
 
 //funtion to find the middle of the list
 struct node *FindMiddle(struct node *head)
 {
+	if(head==NULL)
+		return NULL;
+		
+	struct node *temp=head;
+	
+	int l=0;
+	int h = lengthList(head);
+	int mid = l + (h-l)/2; //finding the middle index in list
+	
+	for(int i = 0 ; i < mid ; i++)
+	{
+		temp = temp->next;
+	}
+	
+	
+	return temp;
+	
+	
+	
 	
 }
 
@@ -44,11 +78,12 @@ void SortedInsert(node **root,int value)
 
 node *newNode(int data)
 {
-	BSTnode *temp = new BSTnode();
+	node *temp = new node();
 	
 	temp->data = data;
 	
 	temp->left = temp->right = NULL;
+	temp->next = NULL;
 	
 	return temp;
 }
@@ -56,5 +91,14 @@ node *newNode(int data)
 
 int main()
 {
+	struct node *head = newNode(1);
+	
+	head->next  = newNode(2);
+	head->next->next = newNode(3);
+	
+	head->next->next->next = newNode(4);
+	
+	cout<<lengthList(head);
+	cout<<FindMiddle(head)->data<<endl;
 	
 }
