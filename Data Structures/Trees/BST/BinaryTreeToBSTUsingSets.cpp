00d6be58210d7af22365eb64c8a7964adf5cea44
anishsingh@20 <loveeanish@gmail.com>
2018-03-21 11:56:48 +0530

    program to convert tree to a BST using sets

---
+++ Data Structures/Trees/BST/BinaryTreeToBSTUsingSets.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+/*program to covert a Binary tree to BST using sets as containers.
+
+Sets in cpp are implemented using self-balancing BST. hence it by default sorts the data.
+
+
+*/
+int main()
+{
+	
+}
