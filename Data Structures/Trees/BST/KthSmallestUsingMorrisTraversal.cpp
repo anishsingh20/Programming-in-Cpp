5c24b6db3bff405137aa325dfe6737a8252a1831
anishsingh@20 <loveeanish@gmail.com>
2018-03-23 02:08:15 +0530

    made the function to find kth smallest using morris inorder traversal

--- Data Structures/Trees/BST/KthSmallestUsingMorrisTraversal.cpp
+++ Data Structures/Trees/BST/KthSmallestUsingMorrisTraversal.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 
 
+struct BSTnode{
+	
+	int data;	
+	struct BSTnode *left,*right;
+};
+
 
 BSTnode *newNode(int data)
 {
@@ -17,6 +23,68 @@ BSTnode *newNode(int data)
 	
 	return temp;
 }
+
+
+BSTnode *KthSmallestUsingMorrisTraversal(BSTnode *root,int k)
+{
+	
+	BSTnode *curr=root;
+	int count = 0;
+	BSTnode *ksmall;
+	BSTnode *pre;
+	
+	while(curr)
+	{
+		//case-1 if current node has no left child
+		if(curr->left==NULL)
+		{
+			if(++count==k)
+				ksmall  = curr;
+			
+			curr = curr->right;
+				
+		}
+		
+		else
+		{
+			//find the inorder predecessor of current node
+			//inorder predecessor is the right most in left subtree i.e
+			//maximum element in left subtree or left child itself
+			pre = curr->left;
+			
+			while(pre->right!=NULL && pre->right!=curr)
+			{
+				pre = pre->right;	
+			}	
+			
+			//create threads
+			if(pre->right==NULL)
+			{
+				pre->right = curr;
+				
+				curr = curr->left;
+				
+			}
+			
+			//if the thread between current and its predecessor already exists.
+			else
+			{
+				pre->right = NULL;
+				
+				if(++count==k)
+					ksmall  = curr;
+					
+				curr = curr->right;	
+					
+			}
+		}
+	}
+	
+	return ksmall;
+	
+	
+}//TIME COMPLEXITY = O(n) and Space complexity = O(1)
+
 
 BSTnode *insert(BSTnode *root,int value)
 {
@@ -60,6 +128,8 @@ int main()
 	insert(root,6);
 	insert(root,12);
 	insert(root,20);
+	
+	cout<<KthSmallestUsingMorrisTraversal(root,1)->data;
 	
 	
 	return 0;
