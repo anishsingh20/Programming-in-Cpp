02ecc60c0459c018c12180ef8422148b0bc532ce
anishsingh@20 <loveeanish@gmail.com>
2018-03-23 01:50:20 +0530

    made the helper functions

--- Data Structures/Trees/BST/KthSmallestUsingMorrisTraversal.cpp
+++ Data Structures/Trees/BST/KthSmallestUsingMorrisTraversal.cpp
@@ -5,7 +5,62 @@
 using namespace std;
 
 
+
+
+BSTnode *newNode(int data)
+{
+	BSTnode *temp = new BSTnode();
+	
+	temp->data = data;
+	
+	temp->left = temp->right = NULL;
+	
+	return temp;
+}
+
+BSTnode *insert(BSTnode *root,int value)
+{
+	//we first need to find the appropiate location to insert data
+	if(root==NULL)
+	{
+		BSTnode *temp  = new BSTnode();
+		temp->data = value;
+		temp->left = temp->right = NULL;
+		
+		root = temp;
+	}
+	
+	
+	else if(root->data > value)
+	{
+		root->left = insert(root->left,value);
+	}
+		
+	else if(root->data < value)
+	{
+		root->right = insert(root->right,value);
+	}
+	
+	
+	
+	//returning root of the updated tree
+	return root;
+}
+
+
+
 int main()
 {
+	BSTnode *root  = newNode(4);
 	
+	insert(root,2);
+	insert(root,3);
+	insert(root,1);
+	insert(root,7);
+	insert(root,6);
+	insert(root,12);
+	insert(root,20);
+	
+	
+	return 0;
 }
