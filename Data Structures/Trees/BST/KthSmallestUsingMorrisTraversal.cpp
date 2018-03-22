98ea83627d13146022f2909abeb1f41d9966f753
anishsingh@20 <loveeanish@gmail.com>
2018-03-23 01:49:12 +0530

    program to find Kth smallest using Morris traversal

---
+++ Data Structures/Trees/BST/KthSmallestUsingMorrisTraversal.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+//Program to find Kth smallest element in a BST using morris traversal to do efficient and faster Inorder traversal which consumes O(1) extra space.
+
+using namespace std;
+
+
+int main()
+{
+	
+}
