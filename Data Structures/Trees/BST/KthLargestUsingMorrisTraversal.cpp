e47236eebc7644aa2931ac419a950b59227bb58c
anishsingh@20 <loveeanish@gmail.com>
2018-03-23 00:26:47 +0530

    Kth largest in BST using morris traversal-efficient technique

---
+++ Data Structures/Trees/BST/KthLargestUsingMorrisTraversal.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+/* FInding Kth largest element in a BST using MORRIS TRAVERAL to do reverse inorder traversal which uses constant extra memory of O(1).
+Reverse inorder traversal implemented using Stack or Recursion both consumes extra memory of order O(n). Hence to improve the efficieny we do traversal
+faster we use Morris traversal to do reverse inorder traversal which is based on THREAED TREES to do faster inorder traversal.
+
+*/
+
+
+int main()
+{
+	return 0;
+}
