edca47bcb7e902c5bc15a788002d70f502f39c24
anishsingh@20 <loveeanish@gmail.com>
2018-03-25 01:24:17 +0530

    program to check if 2 strings are anagrams or not

---
+++ General Programs/CheckAnagrams.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+/*Program to check whether to strings are anagrams or not-
+
+1)Sort the strings.
+2)Them compare the sorted strings.
+
+*/
+
+int main()
+{
+	
+}
