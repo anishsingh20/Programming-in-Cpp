26c96ce2cdaaea95846e25134ba2aaaeb2a4fdda
anishsingh@20 <loveeanish@gmail.com>
2018-03-25 01:07:57 +0530

    made the main heap sort function

--- Data Structures/SORTING/HeapSort.cpp
+++ Data Structures/SORTING/HeapSort.cpp
@@ -40,6 +40,25 @@ void heapify(int arr[],int n,int i)
 } //Time complexity = O(logn), Space complexity = O(n)
 
 
+void HeapSort(int arr[],int n)
+{
+	//build heap in bottom-up manner
+	for(int i = n/2 - 1 ; i >= 0 ;  i--)
+		heapify(arr,n,i);
+		
+	//one by one extract element from heap
+	for(int i= n-1; i>=-;i--)
+	{
+		//swap root and last node
+		swap(arr[0],arr[i]);
+		
+		//calling heapify on reduced heap
+		heapify(arr,i,0);
+	}
+	
+}
+
+
 int main()
 {
 	return 0;
