a0847258baee46439fbdbff663feb5aedbb80985
anishsingh@20 <loveeanish@gmail.com>
2018-03-25 00:54:02 +0530

    added the algorithm to do heap sort

--- Data Structures/SORTING/HeapSort.cpp
+++ Data Structures/SORTING/HeapSort.cpp
@@ -2,4 +2,20 @@
 
 using namespace std;
 
-//HEAP SORT-heap sort is a comparison sorting technique which is based on Binary HEAP which is a form of ordered Complete binary tree.
+/*HEAP SORT-heap sort is a comparison sorting technique which is based on Binary HEAP which is a form of ordered Complete binary tree.
+Algorithm-
+1)Build a heap
+2) Build a MAX-HEAP where the parent node has data larger than its children nodes.
+3) After building the max heap, we simply swap the root and the last node , and delete the last node from HEAP.
+
+
+We will build a BINARY HEAP using array based implementation as it is space efficient.
+
+*/
+
+
+int main()
+{
+	return 0;
+	
+}
