166f22f0d5bf5ef247760e34098a2da043bcd5be
anishsingh@20 <loveeanish@gmail.com>
2018-03-27 00:14:20 +0530

    implemented taylor series to find approx e^x

--- General Programs/TaylorSeries.cpp
+++ General Programs/TaylorSeries.cpp
@@ -5,9 +5,24 @@ using namespace std;
 //program to find e^x using taylor series-
 
 
+//this program for larger values of y, appriximates e^x
+float exp(float x,int y)
+{
+	float p,s; int i;
+	
+	for(s = 1 ,p=1,i=1; i<y ; i++)
+	{
+		 p *= x/i;
+		 s += p;
+	}
+	
+	return s;
+}
+
 
 int main()
 {
+	cout<<exp(10,10);
 	
 }
 
