bd91f170be9f2ed961f309e47aeaa815e6abd4c0
anishsingh@20 <loveeanish@gmail.com>
2018-03-27 02:56:03 +0530

    implemented program to find ceiling and floor in unsorted array

--- General Programs/CeilingAndFloorInUnsortedArray.cpp
+++ General Programs/CeilingAndFloorInUnsortedArray.cpp
@@ -4,4 +4,56 @@
 using namespace std;
 
 
-//Program to find Ceiling and floor in an unsorted array
+/*Program to find Ceiling and floor in an unsorted array
+Algorithm-
+The idea is to traverse array and keep track of two distances with respect to x.
+1) Minimum distance of element greater than or equal to x.
+2) Minimum distance of element smaller than or equal to x.
+Finally print elements with minimum distances.
+
+
+*/
+int f=0 ,c=0;
+
+void floorAndCeil(int *arr,int key, int n)
+{
+    
+    
+ 
+    // Distances of current floor and ceiling
+    int fDist = INT_MAX, cDist = INT_MAX;
+ 
+    for (int i=0; i<n; i++)
+    {
+        // If current element is closer than
+        // previous ceiling.
+        if (arr[i] >= key && cDist > (arr[i] - key))
+        {
+           c = arr[i];
+           cDist = arr[i] - key;
+        }
+ 
+        // If current element is closer than
+        // previous floor.
+        if (arr[i] <= key && fDist > (key - arr[i]))
+        {
+           f = arr[i];
+           fDist = key - arr[i];
+        }
+    }
+    
+    
+}
+
+
+int main()
+{
+	int arr[ ] = {9,1,23,2,10,8};
+	
+	int size = sizeof(arr)/sizeof(arr[0]);
+	
+	floorAndCeil(arr,20,size-1);
+	
+	cout<<"Floor is: "<<f<<endl;
+	cout<<"Ceiling is: "<<c<<endl;
+}
