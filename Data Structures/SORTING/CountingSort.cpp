b3acff0142904da447565780cdcfef0c7352e7ea
anishsingh@20 <loveeanish@gmail.com>
2018-03-26 23:43:20 +0530

    implemented the counting sort

--- Data Structures/SORTING/CountingSort.cpp
+++ Data Structures/SORTING/CountingSort.cpp
@@ -2,7 +2,7 @@
 #include<cstring>
 
 #define RANGE 255
-
+//ascii range
 
 using namespace std;
 
@@ -16,12 +16,45 @@ void CountSort(char *arr)
 	char output[strlen(arr)];
 	
 	//create a count array to store count of all objects
+	//here range represents the ASCII range
 	int count[RANGE+1],i;
+	memset(count, 0, sizeof(count));
+	
+	//store count of each character
+	for(i = 0 ; arr[i];i++)
+	{
+		++count[arr[i]];
+	}
+	
+	//updating count with the sum of previous counts of elements
+	for(i = 1; i<=RANGE;i++)
+	{
+		count[i] += count[i-1]; 
+	}
+	
+	//building the output array
+	for(i=0; arr[i] ; ++i)
+	{
+		output[count[arr[i]]-1] = arr[i];
+		--count[arr[i]];
+	}
+		
+	
+	for(i=0;arr[i];++i)
+		arr[i] = output[i];
+		
 }
 
 
 int main()
 {
+	char c[] = "zdecdea";
+	
+	//sorting a char array within range 0 to 255
+	CountSort(c);
+	
+	cout<<c;
+	
 	
 	
 }
