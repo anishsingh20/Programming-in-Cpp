b838e67169a100adb3f72a0a9b48304c11f55793
anishsingh@20 <loveeanish@gmail.com>
2018-03-26 23:28:08 +0530

    making the countsort function

--- Data Structures/SORTING/CountingSort.cpp
+++ Data Structures/SORTING/CountingSort.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
+#include<cstring>
+
+#define RANGE 255
 
 
 using namespace std;
 
 /*Counting sort which is a non-comparison based sorting technique used fot data which is in a specific range.
 */
+
+
+void CountSort(char *arr)
+{
+	//the output array will have sorted array
+	char output[strlen(arr)];
+	
+	//create a count array to store count of all objects
+	int count[RANGE+1],i;
+}
+
+
+int main()
+{
+	
+	
+}
