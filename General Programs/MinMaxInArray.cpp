bb5066abf66615f77c5d04d3cdd7a14f96d1fa38
anishsingh@20 <loveeanish@gmail.com>
2018-03-27 12:21:59 +0530

    progarm to find min and max in an array

---
+++ General Programs/MinMaxInArray.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+/* Program to find min-max in an array
+1) linear method
+2)Divide and conquer 
+*/
+
+
+struct minMax{
+	
+	int min  = INT_MAX;
+	int max = INT_MIN;
+};
+
+
+//MEthod-1 Linear method with linear comparisons
+struct minMax findMinMax(int *arr,int low,int high)
+{
+	
+}
