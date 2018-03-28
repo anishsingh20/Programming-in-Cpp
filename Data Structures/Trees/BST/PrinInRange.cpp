dfa8e7925a43ff20ae63a9c10600e55ce467b8e1
anishsingh@20 <loveeanish@gmail.com>
2018-03-28 10:58:16 +0530

    program to print data in a range in BST

---
+++ Data Structures/Trees/BST/PrinInRange.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+/*Progrma to print data within a range say K1 and K2 in a BST-ASked in Microsort and Flipkart.
+1)Recursive method-
+2)Level order traverasl method
+3)Threaded tree and inorder successor
+
+
+*/
+int main()
+{
+	return 0;
+}
