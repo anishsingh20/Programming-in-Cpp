77ebbb675abb87da4f38545a29c128577e558906
anishsingh@20 <loveeanish@gmail.com>
2018-03-28 23:53:29 +0530

    added the necessary helper functions

--- Data Structures/Trees/BST/Check2BSTareSameOrNot.cpp
+++ Data Structures/Trees/BST/Check2BSTareSameOrNot.cpp
@@ -2,6 +2,64 @@
 
 using namespace std;
 
+struct BSTnode{
+	
+	int data;	
+	struct BSTnode *left,*right;
+};
 
-/*Program to find if 2 BST are same , i.e have same structures or not.
+
+/*Program to find if 2 BST are same , i.e have same elements or not.
+
 */
+
+
+
+BSTnode *newNode(int data)
+{
+	BSTnode *temp = new BSTnode();
+	
+	temp->data = data;
+	
+	temp->left = temp->right = NULL;
+	
+	return temp;
+}
+
+
+BSTnode *insert(BSTnode *root,int value)
+{
+	//we first need to find the appropiate location to insert data
+	if(root==NULL)
+	{
+		BSTnode *temp  = new BSTnode();
+		temp->data = value;
+		temp->left = temp->right = NULL;
+		
+		root = temp;
+	}
+	
+	
+	else if(root->data > value)
+	{
+		root->left = insert(root->left,value);
+	}
+		
+	else if(root->data < value)
+	{
+		root->right = insert(root->right,value);
+	}
+	
+	
+	
+	//returning root of the updated tree
+	return root;
+}
+
+
+int main()
+{
+	
+	
+	
+}
