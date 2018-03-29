4e287ed6d13000bc19d8a03558391b59bac2e274
anishsingh@20 <loveeanish@gmail.com>
2018-03-30 01:32:43 +0530

    gave the formula for finding nck

--- General Programs/BinomialCoefficient.cpp
+++ General Programs/BinomialCoefficient.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
-//Program to find binomial coefficient C(n,k) i.e n c K-combination
+/*Program to find binomial coefficient C(n,k) i.e n c K-combination
+
+C(n,k) =  n! / (n-k)! . k! = fact(n) / fact(n-k)*fact(k)
+
+
+
+*/
 
 
 int main()
