eb292f594678ef601fdeb9b6bd18a6afab4b9e01
anishsingh@20 <loveeanish@gmail.com>
2018-03-30 00:59:41 +0530

    implemented recursive program to find catalan number

--- Dynamic Programming/CatalanNumber.cpp
+++ Dynamic Programming/CatalanNumber.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 
-using namesapce std;
+using namespace std;
 
 /*Program to calculate nth CATALAN NUMBER-catalan number is simply a sequence of number which has lots of applications in DSA.
 
@@ -13,6 +13,36 @@ Method to calculate CATALAN NUMBER-
 3) Binomial coefficient = C(n) =  1/(n+1) * (2n c n) , linear time algorithm
 4) C(n) = product(n+k/k) , where  k = 2 to n 	, linear time algorithm
 					      
-				 
-
 */
+
+
+//Method-1 Recursion-exponential time algorithm
+long long int CatalanRecursive(int n)
+{
+	long long int res=0;
+	//base case
+	if(n<=1)
+		return 1;
+
+	for(int i = 0; i < n ; i++)
+	{
+		res += CatalanRecursive(i)*CatalanRecursive(n-i-1);
+	}
+	
+	
+	//or we can also do this
+//	for(int i = 0; i < n ; i++)
+//	{
+//		res += CatalanRecursive(i)*CatalanRecursive(n-i-1);
+//	}
+
+	return res;
+
+} //time complexity = exponential, not efficient technique.
+
+
+
+int main()
+{
+	cout<<CatalanRecursive();
+}
