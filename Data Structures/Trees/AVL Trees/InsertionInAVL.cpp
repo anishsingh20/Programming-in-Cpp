46c14e1193eda506e85f95e3d203fb54096e62bb
anishsingh@20 <loveeanish@gmail.com>
2018-03-31 01:53:19 +0530

    made the helper function to insert

--- Data Structures/Trees/AVL Trees/InsertionInAVL.cpp
+++ Data Structures/Trees/AVL Trees/InsertionInAVL.cpp
@@ -17,3 +17,71 @@ struct AVLnode {
 };
 
 
+AVLnode *insert(AVLnode *root,int value)
+{
+	//we first need to find the appropiate location to insert data
+	if(root==NULL)
+	{
+		BSTnode *temp  = new BSTnode();
+		temp->data = value;
+		temp->left = temp->right = NULL;
+		
+		root = temp;
+	}
+	
+	
+	else if(root->data > value)
+	{
+		root->left = insert(root->left,value);
+	}
+		
+	else if(root->data < value)
+	{
+		root->right = insert(root->right,value);
+	}
+	
+	
+	
+	//returning root of the updated tree
+	return root;
+}
+
+
+AVLnode *newNode(int data)
+{
+	AVLnode *temp = new AVLnode();
+	
+	temp->data = data;
+	
+	temp->left = temp->right = NULL;
+	
+	return temp;
+}
+
+void InorderTraverse(AVLnode *root)
+{
+	InorderTraverse(root->left);
+	
+	cout<<root->data<<" ";
+	
+	InorderTraverse(root->right);
+}
+
+
+int main()
+{
+	AVLnode *root  = newNode(4);
+	
+	insert(root,2);
+	insert(root,3);
+	insert(root,1);
+	insert(root,7);
+	insert(root,6);
+	insert(root,12);
+	insert(root,20);
+	
+	
+	
+	
+}
+
