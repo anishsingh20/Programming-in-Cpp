73455616f7f3845c7f1d4e183d17f84491fa20d0
anishsingh@20 <loveeanish@gmail.com>
2018-03-31 01:38:29 +0530

    progarm to do double rotations

---
+++ Data Structures/Trees/AVL Trees/DoubleRightLeftRotation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include "SingleLLRotation.cpp"
+#include "SIngleRRrotation.cpp"
+
+using namespace std;
+
+
+struct AVLnode {
+	
+	int data,height;
+	struct AVLnode *left,*right;	
+};
+
+
+
+AVLnode *DoubleRightLeftRotation(AVLnode *X)
+{
+
+	
+}
