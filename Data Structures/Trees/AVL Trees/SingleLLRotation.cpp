80537eb6935a91a896b25333b1a033abef1bdb95
anishsingh@20 <loveeanish@gmail.com>
2018-03-30 13:42:26 +0530

    AVL trees and single LL rotation

---
+++ Data Structures/Trees/AVL Trees/SingleLLRotation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+/*Program to do Single Left left rotation for converting an unbalaned AVL tree to a balanced AVL Tree when SOME OPERATION such as
+INSERTION OR DELETION happens.
+*/
+
+
+int main()
+{
+	
+}
