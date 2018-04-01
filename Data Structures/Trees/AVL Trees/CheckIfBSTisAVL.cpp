96f8ab7954d22b21aad7ae818fab01ec398fbfbc
anishsingh@20 <loveeanish@gmail.com>
2018-04-02 00:20:14 +0530

    made the helper function

--- Data Structures/Trees/AVL Trees/CheckIfBSTisAVL.cpp
+++ Data Structures/Trees/AVL Trees/CheckIfBSTisAVL.cpp
@@ -3,6 +3,7 @@
 
 
 //program to check is a BST is AVL tree or not
+//we simply have to check for the balance condition recursively while traversing the tree
 
 using namespace std;
 
@@ -12,3 +13,57 @@ struct BSTnode {
 	struct BSTnode *left,*right;	
 };
 
+
+
+int Height(AVLnode *root)
+{
+	if(!root)
+		return -1;
+		
+	else
+		return max(Height(root->left),Height(root->right)) + 1;
+}
+
+
+int isBSTAVL(BSTnode *root)
+{
+	if(!root)
+		return 1;
+	
+	if(Height(root->right)-Height(root->left)==2)
+		return 0;
+		
+	if(root->left)
+		return isBSTAVL(root->left);
+		
+	if(root->right)
+		return isBSTAVL(root->right);
+		
+		
+	return 1;
+}
+
+
+BSTnode *newNode(int data)
+{
+	BSTnode *temp = new BSTnode();
+	
+	temp->data = data;
+	
+	temp->left = temp->right = NULL;
+	
+	return temp;
+}
+
+
+int main()
+{
+	BSTnode *root=newNode(7);
+
+	root->left = newNode(5);
+	root->right = newNode(9);
+	root->right->left = newNode(8);
+	root->left->left = newNode(1);
+	root->left->right = newNode(6);
+	root->right->right = newNode(11);
+}
