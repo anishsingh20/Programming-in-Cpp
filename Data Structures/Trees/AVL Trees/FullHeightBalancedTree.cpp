f9ce681b82cd6ec3630045b0fc527386d409f531
anishsingh@20 <loveeanish@gmail.com>
2018-04-02 11:55:26 +0530

    given height generating FUll balanced binary tree

---
+++ Data Structures/Trees/AVL Trees/FullHeightBalancedTree.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+//Program to generate a full height balanced binary tree where balance factor is atmost 0 for all nodes.
+//balance factor(k) = height(left subtree)  - height(right subtree)
+
+
+struct Node {
+	
+	int data;
+	struct Node *left,*right;
+};
+
+
+int main()
+{
+	
+}
