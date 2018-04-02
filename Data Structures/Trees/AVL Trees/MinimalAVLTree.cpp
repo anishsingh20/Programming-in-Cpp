b59be038b8024b364c8f460d7c6694f1a74eb84a
anishsingh@20 <loveeanish@gmail.com>
2018-04-03 01:17:00 +0530

    program to build an minimal AVL tree of height h

---
+++ Data Structures/Trees/AVL Trees/MinimalAVLTree.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+/*Program to find Minimum number of nodes in an AVL tree of height h i.e generating an MINIMAL AVL tree
+
+We will utilize the recurrence relation
+N(h) = 1+ N(h-1) + N(h-2)  are minimum number of nodes in an AVL tree of height h 
+
+
+*/
+
+
+int main()
+{
+	
+}
