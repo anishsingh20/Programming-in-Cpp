6d1166c59d738d205f34daabc57033a6ea194705
anishsingh@20 <loveeanish@gmail.com>
2018-04-02 12:16:02 +0530

    Explained time complexity of the function

--- Data Structures/Trees/AVL Trees/FullHeightBalancedTree.cpp
+++ Data Structures/Trees/AVL Trees/FullHeightBalancedTree.cpp
@@ -13,7 +13,47 @@ struct Node {
 };
 
 
+
+//nodes will be from 1 to 2^(h+1) - 1. As in a full binary tree max number of nodes can be 2^(h+1) - 1, if we assume height of a tree with
+//single node = 0  
+
+//the function takes parameter as height of tree
+Node *FullBalancedBST(int h)
+{
+	static int count=1;
+	Node *temp = new Node();
+	
+	if(h==-1)
+		return NULL;
+	
+	temp->left = FullBalancedBST(h-1);
+	
+	temp->data = count++;
+	
+	temp->right = FullBalancedBST(h-1);
+	
+	return temp;
+	
+}//time complexity = O(n) and Space complexity = O(logn) as stack space consumed is proportional to the height of a recursion tree.
+//and in this case it is a FUll binary search tree where height h = logn
+
+void Inorder(Node *root)
+{
+	if(!root)
+		return;
+		
+	Inorder(root->left);
+	
+	cout<<root->data<<" ";
+	
+	Inorder(root->right);
+}
+
 int main()
 {
+	
+	struct Node *root = FullBalancedBST(4);
+	
+	Inorder(root);
 	
 }
