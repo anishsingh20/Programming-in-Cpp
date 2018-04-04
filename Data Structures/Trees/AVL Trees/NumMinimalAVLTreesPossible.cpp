6a0a07a4f8db624591f01cf6ef488da8fb40b4de
anishsingh@20 <loveeanish@gmail.com>
2018-04-04 10:23:19 +0530

    explained the recurrence relation to find MInimal AVL trees possible

--- Data Structures/Trees/AVL Trees/NumMinimalAVLTreesPossible.cpp
+++ Data Structures/Trees/AVL Trees/NumMinimalAVLTreesPossible.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 /*Program to find number of different structured minimal AVL trees i,e AVL tree with minimum nodes.
+
+We know to find minimum no nodes in an AVL tree is given by N(h) = 1 + N(h-1) + N(h-2).
+
+In simialr manner number of different minimal AVL trees is given by the below recurrence relation
+
+NT(h) = 2*NT(h-1)*NT(h-2) , NT(h) are no of Minimal AVL trees possible
+
 */
 
 
