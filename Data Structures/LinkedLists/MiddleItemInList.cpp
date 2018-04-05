8e63608d5ae903e616fbdb8a77b2cb2aea3b7892
anishsingh@20 <loveeanish@gmail.com>
2018-04-05 10:24:41 +0530

    program to find middle item in a linked list

---
+++ Data Structures/LinkedLists/MiddleItemInList.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+//SINGLY LINKED LIST
+using namespace std;
+
+struct node {
+	int data;
+	struct Node *next;
+};
+
+
+
+
+struct node* midItem(struct node* head)
+{
+	node *temp,*temp1;
+	if(!head)
+		return NULL;
+	temp = head;
+	temp1= head;//pointer which moves 2 steps
+		
+	if(temp)
+	{
+		midItem(temp->next);
+		if(temp1->next!=NULL)
+			return midItem(temp1->next->next);
+			
+		if(temp1->next==NULL)
+			return temp;
+		
+		
+	}
+}
