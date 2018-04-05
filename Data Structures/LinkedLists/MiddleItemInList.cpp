e194e3a30c53967dfa0b5eb103437797750f00a7
anishsingh@20 <loveeanish@gmail.com>
2018-04-05 10:42:03 +0530

    implemented 2 different methods to find middle element in a linked list

--- Data Structures/LinkedLists/MiddleItemInList.cpp
+++ Data Structures/LinkedLists/MiddleItemInList.cpp
@@ -5,40 +5,83 @@ using namespace std;
 
 struct node {
 	int data;
-	struct Node *next;
+	struct node *next;
 };
 
 
 
 
-struct node* midItem(struct node* head)
+node *midItem(struct node **head)
 {
-	node *temp,*temp1;
+	struct node *temp,*temp1;
 	if(!head)
 		return NULL;
-	temp = head;
-	temp1= head;//pointer which moves 2 steps
+	temp = *head;
+	temp1 = *head;//pointer which moves 2 steps
 		
-	if(temp && temp1)
+	while(temp!=NULL && temp1->next != NULL)
 	{
-		midItem(temp->next);
-		if(temp1->next!=NULL)
-			return midItem(temp1->next->next);
-			
-		if(temp1->next==NULL)
-			return temp;
+		
+		temp = temp->next;
+		temp1 = temp1->next->next;
 		
 		
 	}
+	
+	if(temp)
+		return temp;
+			
+			
+	
+		
+		
+	
+}
+
+
+
+//finding length of a List
+int length(struct node *head) {
+	
+	int len = 0;
+	while(head!=NULL) {
+		len++;
+		head = head->next;
+	}
+	
+	return len;
+	
+}
+
+//function to get the Middle of a list
+int MiddleNode(struct node **head,int start=0) 
+{
+	
+	
+	if(*head==NULL) 
+	{
+		cout<<"list enpty"<<endl;
+	}
+	
+	int len = length(*head);
+	int mid = (start + (len-start)/2 );
+	struct node *temp=*head;
+	for(int i=0;i<mid;i++)
+		temp=temp->next;
+		
+	return temp->data; 
+
 }
 
 
 struct node *newNode(int data)
 {
 	node *temp = new node();
-	temp->next=NULL;
+
 	
 	temp->data = data;
+	
+	temp->next = NULL;
 	
 	return temp;
 }
@@ -46,4 +89,20 @@ struct node *newNode(int data)
 int main()
 {
 	node *head = newNode(1);
+	
+	head->next = newNode(2);
+	head->next->next = newNode(3);
+	head->next->next->next = newNode(4);
+	head->next->next->next->next = newNode(5);
+//	head->next->next->next->next->next = newNode(6);
+	
+	
+	cout<<midItem(&head)->data;
+	
+	
+	cout<<endl;
+	
+	cout<<MiddleNode(&head);
+	
+	
 }
