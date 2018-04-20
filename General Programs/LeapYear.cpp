08116885bb7ebb95c99764809528dbbb25235493
Anish Singh Walia <loveeanish@gmail.com>
2018-04-20 20:23:29 +0530

    added some definition about leap year

--- General Programs/LeapYear.cpp
+++ General Programs/LeapYear.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-
-//program to find whether a year is leap year or not.
-//A year is a leap year if it is divisible by 4 , and if the year is evenly divisible by 100, then is is not a leap year, unless it is divisible by 400.
-//if the year is divisible by 400, then it is a leap year.
-
+/*A leap year is-
+program to find whether a year is leap year or not.
+A year is a leap year if it is divisible by 4 , and if the year is evenly divisible by 100, then is is not a leap year, unless it is divisible by 400.
+if the year is divisible by 400, then it is a leap year.
+*/
 bool leapOrNot(long int year)
 {
 	
