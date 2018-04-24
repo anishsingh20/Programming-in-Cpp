528f89d1b17e812db231e1a344eb31c6d47923a2
anishsingh@20 <loveeanish@gmail.com>
2018-04-24 09:01:24 +0530

    made a function for prefix evaluation

--- Data Structures/Stacks/PostfixEval.cpp
+++ Data Structures/Stacks/PostfixEval.cpp
@@ -10,11 +10,12 @@ using namespace std;
 //declaring functions
 
 //1) function to get the result
+double getResult(string postfix) ;
 
-int getResult(string postfix);
+double getResultPrefix(string prefix);
 
 //function to perform the operation between the 2 popped operands
-int performOps(char opt,int opd1,int opd2);
+double performOps(char opt,double opd1,double opd2);
 
 //check if the scanned int is an operand
 bool isOperand(char opd);
@@ -23,14 +24,17 @@ bool isOperand(char opd);
 bool isOperator(char c);
 
 
-
-int getResult(string postfix) 
+//for postfix expression evaluation
+double getResult(string postfix) 
 {
 	//defining a stack from STL
-	stack<int> S;
+	stack<double> S;
 	
 	//scanning form left to right of the expression
-	for(int i = 0; i< postfix.length();i++) {
+	for(int i = 0; i < postfix.length();i++) {
+
+	
+	
 		
 		//if delimitter then continue
 		if(postfix[i]== ' ' || postfix[i]==',') continue;
@@ -40,11 +44,11 @@ int getResult(string postfix)
 		else if(!S.empty() && isOperator(postfix[i])) {
 			
 			//pop 2 operands
-			int operand1 = S.top() ; S.pop();
-			int operand2 = S.top() ; S.pop();
+			double operand1 = S.top() ; S.pop();
+			double operand2 = S.top() ; S.pop();
 			
 			//performing the operation and getting result
-			int result = performOps(postfix[i],operand1,operand2);
+			double result = performOps(postfix[i],operand1,operand2);
 			
 			//pushing the result to stack
 			S.push(result);
@@ -74,7 +78,63 @@ int getResult(string postfix)
 }
 
 
-int performOps(char opt,int opd1,int opd2) {
+
+//for prefix evaluation, we scan from the right to left
+	
+double getResultPrefix(string prefix) 
+{
+	//defining a stack from STL
+	stack<double> S;
+	
+
+	//for prefix evaluation, we scan from the right to left i.e the operands
+	for(int i = prefix.size()-1; i >= 0 ;i--)
+	{
+	
+		
+		//if delimitter then continue
+		if(prefix[i]== ' ' || prefix[i]==',') continue;
+		
+		//if operator is scanned , then pop 2 operands from top of stack and 
+		//perform the ops and push the result back to stack
+		else if(!S.empty() && isOperator(prefix[i])) {
+			
+			//pop 2 operands
+			double operand1 = S.top() ; S.pop();
+			double operand2 = S.top() ; S.pop();
+			
+			//performing the operation and getting result
+			double result = performOps(prefix[i],operand1,operand2);
+			
+			//pushing the result to stack
+			S.push(result);
+			
+			
+			
+		}
+		
+		
+	//if the scanned element is a operand-push it to stack
+		else if (isOperand(prefix[i])) {
+			//extract numeric operand from expression
+			int operand = 0;
+			//checking if the scanned expression is digit
+			if(isdigit(prefix[i])) {
+				//the below expression converts string operand to integer operant to do calculation 
+				operand = (prefix[i]-'0');
+			}
+			
+			//pushing operand on to stack
+			S.push(operand);
+		}
+	}
+	
+	//top of stack contains the result so we return it.
+	return S.top(); //result stored at top of stack
+}
+
+
+double performOps(char opt,double opd1,double opd2) {
 	
 	if(opt=='+') return opd1+opd2;
 	if(opt=='-') return abs(opd1-opd2);
@@ -89,7 +149,7 @@ int performOps(char opt,int opd1,int opd2) {
 
 
 bool isOperator(char c) {
-	if(c == '+' || c == '-' || c == '*' || c=='/' || c ==' $' )
+	if( c == '+' || c == '-' || c == '*' || c=='/' )
 		return true;
 		
 		
@@ -107,12 +167,13 @@ bool isOperand(char opd) {
 
 
 
-int main(){
+int main()
+{
 	string expression;
-	cout<<"Enter the postfix expression:"<<endl;
+	cout<<"Enter the prefix expression:"<<endl;
 	getline(cin,expression);
 	
-	int result = getResult(expression);
+	double result = getResultPrefix(expression);
 	
 	cout<<"The result of "<<expression<<  " is :"<<result<<endl;
 	return 0;
