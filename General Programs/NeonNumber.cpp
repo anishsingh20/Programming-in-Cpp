74e827a06ebf6274ec3606c91400834c34f491a2
anishsingh@20 <loveeanish@gmail.com>
2018-04-27 19:13:11 +0530

    build program to find if a number is neon or not

--- General Programs/NeonNumber.cpp
+++ General Programs/NeonNumber.cpp
@@ -3,15 +3,42 @@
 using namespace std;
 
 
-//Program to find NEON NUMBER
+//Program to find NEON NUMBER- A neon number is a nunber if the sum of digits of the square of the number n equals itself.
+
+//eg-9 , sq(9) = 81, 8+1 = 9
 
 bool NeonNumber(int n)
 {
+	int sum=0;
+	int sq = (n*n); //square of number
+	int digit;
+	//finding digits of number
+	
+	while(sq != 0)
+	{
+	 	int digit = sq%10;
+	 	sq = sq /10;
+	 	sum += digit;
+		
+				
+		
+	}
+	
+	//if the sum of digits equals the number itself then it is a neon number
+	if(sum == n)
+	{
+		return true;
+	}
+	
+	return false;
+
+	
 	
 }
 
 
 int main()
 {
+	cout<<NeonNumber(9);
 	
 }
