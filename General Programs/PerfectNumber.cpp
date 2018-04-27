b18c9b1fc89d567d73dc34269af830c7e3617778
anishsingh@20 <loveeanish@gmail.com>
2018-04-27 19:23:33 +0530

    made function to check if a number if perfect or not

--- General Programs/PerfectNumber.cpp
+++ General Programs/PerfectNumber.cpp
@@ -9,9 +9,30 @@ using namespace std;
 //eg-6, its factors= 1,2,3 = 3+2+1=6
 
 //to check if a number is a perfect number or not
-bool PerfectNumber()
+bool PerfectNumber(int n)
 {
+	int factors=1;
+	int sum = 0;
 	
+	while(n > factors)
+	{
+		//if n is divisible by factors
+		if(n%factors==0)
+		{
+			sum += factors;
+						
+		}
+		
+		//otherwise simply increment and again check
+		factors++;
+	}
+	
+	if(sum==n)
+	{
+		return true;
+	}
+	
+	return false;
 	
 }
 
@@ -23,6 +44,7 @@ void PerfectNumbersInRange(int range)
 
 int main()
 {
+	cout<<PerfectNumber(6);
 	
 }
 
