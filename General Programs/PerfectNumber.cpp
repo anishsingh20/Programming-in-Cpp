81cd566b51ad73803d69528070a25d75494e50a7
anishsingh@20 <loveeanish@gmail.com>
2018-04-27 19:18:57 +0530

    program to find Perfect numbers

--- General Programs/PerfectNumber.cpp
+++ General Programs/PerfectNumber.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
 
+
 using namespace std;
 
 
-//Program to find PERFECT NUMBER
+//Program to find PERFECT NUMBER- a perfect number is a number whose factors leaving itself when added equals to the number itself.
+
+//eg-6, its factors= 1,2,3 = 3+2+1=6
+
+//to check if a number is a perfect number or not
+bool PerfectNumber()
+{
+	
+	
+}
+
+void PerfectNumbersInRange(int range)
+{
+	
+}
+
+
+int main()
+{
+	
+}
 
