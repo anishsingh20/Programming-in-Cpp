79bc9f85ca1c7f4e9bf87d1de886ef3adc3785a2
anishsingh@20 <loveeanish@gmail.com>
2018-04-28 10:08:11 +0530

    making a function to do desired operations

--- General Programs/XORoperations.cpp
+++ General Programs/XORoperations.cpp
@@ -4,4 +4,17 @@
 using namespace std;
 
 
-//Program demonstarting the XOR relation a XOR b = c, then to find sq(a) XOR sq(b) = c, then we can find b = sqrt(a XOR b)
+//Program demonstarting the XOR relation a XOR b = c, then to find sq(a) XOR sq(b) = c, then we can find b = sqrt( sq(a) XOR b )
+
+int XORops(int a,int c)
+{
+	int sq = (a*a);
+	
+	int pro = (sq ^ c);
+}
+
+
+int main()
+{
+	
+}
