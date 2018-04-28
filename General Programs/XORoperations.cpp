a469b36a9a498063f7f3fcc8c45abcb308a7e67e
anishsingh@20 <loveeanish@gmail.com>
2018-04-28 10:17:57 +0530

    added some explanation

--- General Programs/XORoperations.cpp
+++ General Programs/XORoperations.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 #include<math.h>
 
+//Program demonstarting the XOR relation a XOR b = c, then to find sq(a) XOR sq(b) = c, then we can find b = sqrt( sq(a) XOR b )
+//eg- 11*11 XOR 12*12 = 233, then sqrt(11*11  XOR 233) = 12
+
 using namespace std;
 
 
-//Program demonstarting the XOR relation a XOR b = c, then to find sq(a) XOR sq(b) = c, then we can find b = sqrt( sq(a) XOR b )
-//eg- 11*11 XOR 12*12 = 233, then sqrt(11*11  XOR 233) = 12
+
 
 int XORops(int a,int c)
 {
