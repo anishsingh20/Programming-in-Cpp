3a5a1d66e919453999cf31645075af1ab209999e
anishsingh@20 <loveeanish@gmail.com>
2018-04-28 10:15:29 +0530

    made the program and successfully ran

--- General Programs/XORoperations.cpp
+++ General Programs/XORoperations.cpp
@@ -1,20 +1,23 @@
 #include<iostream>
-
+#include<math.h>
 
 using namespace std;
 
 
 //Program demonstarting the XOR relation a XOR b = c, then to find sq(a) XOR sq(b) = c, then we can find b = sqrt( sq(a) XOR b )
+//eg-
 
 int XORops(int a,int c)
 {
 	int sq = (a*a);
 	
 	int pro = (sq ^ c);
+	
+	return sqrt(pro);
 }
 
 
 int main()
 {
-	
+	cout<<XORops(12,233);
 }
