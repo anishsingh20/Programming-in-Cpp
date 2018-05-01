aa67921249283cc230d8d01c0ada6fc23e307721
anishsingh@20 <loveeanish@gmail.com>
2018-05-01 10:17:50 +0530

    defined the main function

--- Data Structures/Stacks/PrefixEvaluation.cpp
+++ Data Structures/Stacks/PrefixEvaluation.cpp
@@ -3,7 +3,7 @@
 
 
 /*Program to do Prefix expression evaluation using stack
-It is similar to the Postfix evaluation using stack, in this case we just have to read the expression from left to right
+It is similar to the Postfix evaluation using stack, in this case we just have to read the expression from right to left;
 
 */
 
@@ -17,6 +17,33 @@ bool isOperand(char c);
 
 bool isOperator(char c);
 
+int PerfromOps(int a,int b ,char c);
+
+int getResultPre(string exp)
+{
+	stack<int>S;
+	//scanning the expression from right to left
+	for(int i=exp.length();i > 0 ;i--)
+	{
+		if(exp[i]=="," || exp[i]==" ") continue;
+		
+		//if the scanned input is an operator, then pop 2 operands from stack and operate them
+		else if(isOperator(exp[i]) && !s.empty())
+		{
+			int a = S.top(); S.pop();
+			
+			int b = S.top() ; S.pop();
+			
+			int result  = PerformOps(a,b,exp[i]);
+			
+			//pusing the result in stack
+			S.push(result);
+			
+			
+		}
+		
+	}
+}
 
 
 int main()
