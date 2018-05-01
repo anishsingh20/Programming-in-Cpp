d2191839a13e9b1a43db6bec84552d34d299b789
anishsingh@20 <loveeanish@gmail.com>
2018-05-01 09:54:48 +0530

    program to do prefix evaluation

---
+++ Data Structures/Stacks/PrefixEvaluation.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<stack>
+
+
+/*Program to do Prefix expression evaluation using stack
+It is similar to the Postfix evaluation using stack, in this case we just have to read the expression from left to right
+
+*/
+
+using namespace std;
