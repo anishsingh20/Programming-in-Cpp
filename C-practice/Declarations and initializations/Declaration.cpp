bd3d792fc88541255592337334d873d807deb7d6
anishsingh@20 <loveeanish@gmail.com>
2018-05-02 12:16:06 +0530

    difference between declaration and definition

--- C-practice/Declarations and initializations/Declaration.cpp
+++ C-practice/Declarations and initializations/Declaration.cpp
@@ -5,7 +5,9 @@
 
 int main()
 {
+	extern int a;//declaration for a 
+	printf("%d",a); //prints 20
 	return 0;
 }
 
-int a  = 20;
+int a  = 20;//definition of a, mamory and value initialized and allocated
