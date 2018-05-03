96b303a6ad7c5641e8788bacc7650aee3edc2f4a
anishsingh@20 <loveeanish@gmail.com>
2018-05-03 08:05:17 +0530

    control flow in C

---
+++ C-practice/Control Flow/ControlFlow.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+
+
+
+
+int main()
+{
+	int i = 4;
+	
+	switch(i) {
+		
+		default :
+			printf("Hello this is default case");
+			
+			
+		case 1:
+			printf("This is case 1");
+			break;
+			
+		case 2:
+			printf("This is case 2");
+			break;
+	}
+}
+
+
+
