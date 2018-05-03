ce444ebd0a2fc62ee68bc19dcc325f564b323d09
anishsingh@20 <loveeanish@gmail.com>
2018-05-03 08:07:37 +0530

    some minor changes to the program

--- C-practice/Control Flow/ControlFlow.c
+++ C-practice/Control Flow/ControlFlow.c
@@ -7,23 +7,33 @@
 
 int main()
 {
-	int i = 4;
-	
-	switch(i) {
-		
-		default :
-			printf("Hello this is default case");
-			
-			
-		case 1:
-			printf("This is case 1");
-			break;
-			
-		case 2:
-			printf("This is case 2");
-			break;
-	}
-}
+//	int i = 4;
+//	
+//	switch(i) {
+//		
+//		default :
+//			printf("Hello this is default case");
+//			
+//			
+//		case 1:
+//			printf("This is case 1");
+//			break;
+//			
+//		case 2:
+//			printf("This is case 2");
+//			break;
+//	}
+	int i=1;
+	for(;;)
+ 	{
+ 		printf("%d\n",i);
+ 		i++;
+ 		if(i > 10)	
+ 			break;
+	 
+	 	
+	 }
+ }
 
 
 
