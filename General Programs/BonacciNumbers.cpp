8fc5bcd7e981dec9923857656d3fdb62ca062881
anishsingh@20 <loveeanish@gmail.com>
2018-05-10 13:11:38 +0530

    added some explanation about Binacci series

--- General Programs/BonacciNumbers.cpp
+++ General Programs/BonacciNumbers.cpp
@@ -3,10 +3,19 @@
 
 using namespace std;
 
-//Program to print N-Bonacci numbers.
+/*Program to print N-Bonacci numbers.
+ when N=2 , then it becomes Fibonacci, ie sum of previous 2 numbers,
+ when N = 3 the series becomes Tri-Fibonacci, i.e the next number is the sum of previous 3 numbers
+ eg. N =3 , 0 0 1 1 2 4 7 13 24 .. .. 
+
+*/
+int BonacciSeries(int n , int m)
+{
+	
+}
 
 
 int main()
 {
-	
+	cout<<BonacciSeries(6,3);
 }
