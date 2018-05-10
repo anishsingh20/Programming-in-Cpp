fb21fb48a123e649266b9eafe96fd0de849c7894
anishsingh@20 <loveeanish@gmail.com>
2018-05-10 10:08:33 +0530

    important things about storing func return values added

--- C-practice/Miscellaneous/FuncCalls.c
+++ C-practice/Miscellaneous/FuncCalls.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 #include<conio.h>
 
+int func1(int);
 
 int main()
 {
+	int k = 35;
+	k = func1(func1(func1(k))); // k becomes 37
+	// k = func1(k = func1(k = func1(k))); k becomes 38 as the result is stored in k
+	//k = func1(func1(func1(k))); also outputs 38
 	
-	
+	printf("%d",k);
 	return 0;
+}
+
+int func1(int n)
+{
+	n++;
+	return n;
 }
