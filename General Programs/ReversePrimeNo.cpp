e1d5419c38d8a3a0efe246f9b860b368ceb49169
anishsingh@20 <loveeanish@gmail.com>
2018-05-11 00:10:18 +0530

    making a function

---
+++ General Programs/ReversePrimeNo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+
+
+bool ReversePrime(int n)
+{
+	vector<int>v; //vector to store the prime numbers
+	int i=2;
+	
+	
+}
+
+
+int main()
+{
+	return 0;
+}
