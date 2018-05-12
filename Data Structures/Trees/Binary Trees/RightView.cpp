dda76e346972db2463777be0248f32c93b33996b
anishsingh@20 <loveeanish@gmail.com>
2018-05-13 00:10:00 +0530

    right view level order solution implemented

--- Data Structures/Trees/Binary Trees/RightView.cpp
+++ Data Structures/Trees/Binary Trees/RightView.cpp
@@ -22,16 +22,24 @@ void RightView(node *root)
 	queue<node*>q;
 	
 	q.push(root);
-	
 	while(!q.empty())
 	{
 		temp = q.front();
 		q.pop();
 		
+	
+		
+		
 		cout<<temp->data<<" ";
 		
+		//pusing only the right child of current node
 		if(temp->right)
 			q.push(temp->right);
+			
+			
+		//if the current node has no right child, the left child is the only node
+		if(!temp->right)
+			cout<<temp->left->data<<" ";
 			
 			
 	}
@@ -48,17 +56,28 @@ node *newNode(int data)
 
 int main()
 {
-	node *root = newNode(1);
+//	node *root = newNode(1);
+//	
+//	root->left = newNode(2);
+//	
+//	root->right = newNode(3);
+//	
+//	root->left->left = newNode(4);
+//	root->left->right = newNode(5);
+//	root->right->left = newNode(6);
+//	root->right->right = newNode(7);
+//	root->right->right->left = newNode(8);
 	
-	root->left = newNode(2);
 	
-	root->right = newNode(3);
-	
-	root->left->left = newNode(4);
-	root->left->right = newNode(5);
-	root->right->left = newNode(6);
-	root->right->right = newNode(7);
-	root->right->right->right = newNode(8);
+    node* root = newNode(10);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(7);
+    root->left->right = newNode(8);
+    root->right->right = newNode(15);
+    root->right->left = newNode(12);
+    root->right->right->left = newNode(14);
+ 
 	
 	
 	RightView(root);
