66cb23e24d43bf5e67a10c883f5284ccc7327e56
anishsingh@20 <loveeanish@gmail.com>
2018-05-13 18:44:45 +0530

    new program on void pointers

--- C-practice/Pointers/pointer.c
+++ C-practice/Pointers/pointer.c
@@ -13,24 +13,43 @@ int main()
 	
 	//program 2
 	
-	int a[3] = {3,5,6};
+//	int a[3] = {3,5,6};
+//	
+//	char *ptrc = (char*)a;
+//	
+//	printf("%d ",*ptrc);
+//	printf("%d \n",*ptrc + 1); //prints 4, value incremented by 1
+//	
+//	ptrc = ptrc + 1; // or *ptr++; //address incremented to next byte
+//	printf("%d \n", *ptrc); //value is 0
+//	
+//	int arr[5];
+//	char arrc[5];	
+//	printf("%d %d\n",sizeof(arr),sizeof(&arr)); //&arr is the size of pointer 8 bytes
+//	
+//	printf("%d %d\n",sizeof(arrc),sizeof(&arrc));//prints 5 and 8 as c char
+//	
+//	int b = 5;
+//	printf("%d %d",sizeof(b),sizeof(&b)); // prints 4 and 8
 	
-	char *ptrc = (char*)a;
+	//program 3
 	
-	printf("%d ",*ptrc);
-	printf("%d \n",*ptrc + 1); //prints 4, value incremented by 1
 	
-	ptrc = ptrc + 1; // or *ptr++; //address incremented to next byte
-	printf("%d \n", *ptrc); //value is 0
+	float a=10;
+	float *j;
 	
-	int arr[5];
-	char arrc[5];	
-	printf("%d %d\n",sizeof(arr),sizeof(&arr)); //&arr is the size of pointer 8 bytes
+	void *k;
 	
-	printf("%d %d\n",sizeof(arrc),sizeof(&arrc));//prints 5 and 8 as c char
+	k = &a;
 	
-	int b = 5;
-	printf("%d %d",sizeof(b),sizeof(&b)); // prints 4 and 8
+	j = (float*)k;
+	
+	printf("%f", *j);
+
+	
+	
+	
+	
 	
 	return 0;
 	
