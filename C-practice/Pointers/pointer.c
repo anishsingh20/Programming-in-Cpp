e772ff5a97c6f16ab219f09c1017ea203f2574d2
anishsingh@20 <loveeanish@gmail.com>
2018-05-15 19:50:20 +0530

    new program on char pointer

--- C-practice/Pointers/pointer.c
+++ C-practice/Pointers/pointer.c
@@ -107,7 +107,22 @@ int main()
 		
 	//program 8
 	
+	void *vp;
+	char ch = 74, *cp = "JACK";
 	
+	int j = 65;
+	
+	vp = &ch;
+	
+	printf("%c",*(char*)vp);
+	
+	vp = &j;
+	
+	printf("%c",*(char*)vp);
+	
+	vp = cp;
+	
+	printf("%s",(char*)vp+2); //imp-will print CK
 	
 
 	
