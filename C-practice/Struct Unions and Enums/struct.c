b7e717380cc44000ed50433a92179e35af7c3d92
anishsingh@20 <loveeanish@gmail.com>
2018-05-19 11:08:38 +0530

    use of pregma directive

--- C-practice/Struct Unions and Enums/struct.c
+++ C-practice/Struct Unions and Enums/struct.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 #include<conio.h>
 
+#pragma  pack(1)
+
+//pragma directive indicated that structure should be aligned at address which are multiples of 1 , so all data types begin
+//at next immediate address
 struct st{
 	
 	double d;
-	float f;
-	int data;
-	char c;
+	float f;// 8 bytes padded
+	int data;//4 bytes
+	char c;// 4 bytes, 3 bytes padded after 1 byte of c
 }s;
 
 int main()
