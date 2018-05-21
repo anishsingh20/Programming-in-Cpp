37ccc115ebb559bf935a56b5f3c0ecc86d8570cb
anishsingh@20 <loveeanish@gmail.com>
2018-05-21 11:46:50 +0530

    program made

--- General Programs/ABCDPyramid.cpp
+++ General Programs/ABCDPyramid.cpp
@@ -6,5 +6,27 @@ using namespace std;
 
 int main()
 {
+	int n;
+	
+	
+	cout<<"Enter the limit"<<endl;
+	cin>>n;
+	
+	char ch[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'}; 
+	
+	for(int i = 0 ; i <= n ; i++ )
+	{
+		cout<<ch[i]<<endl;
+		for(int j = 0 ; j <= i ; j++)
+		{
+			
+			cout<<ch[j];
+			
+		}
+		
+	}
+	
+		
+	
 	return 0;
 }
