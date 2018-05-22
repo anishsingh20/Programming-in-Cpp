8727e1b1cf0613c411386c1beffd1eac67bbe657
anishsingh@20 <loveeanish@gmail.com>
2018-05-23 02:43:39 +0530

    made the helper funtions

--- Data Structures/Trees/BST/SumOfNodesInRange.cpp
+++ Data Structures/Trees/BST/SumOfNodesInRange.cpp
@@ -4,6 +4,46 @@
 //program to find sum of nodes within a range L and H , for a BST
 using namespace std;
 
+
+struct node {
+	int data;
+	struct node *left,*right;	
+		
+};
+
+
+struct node *newNode(int data)
+{
+	struct node *temp = new node();
+	temp->data = data;
+	
+	return temp;
+}
+
+
+truct node* insert(node *root,int data)
+{
+	if(root==NULL)
+	{
+		return newNode(data);
+	}
+	
+	if(root->data > data)
+	{
+		root->left = insert(root->left,data);
+		
+	}
+	
+	if(root->data < data)
+	{
+		root->right = insert(root->right,data);
+	}
+		
+	return root;
+	
+}
+
+
 int main()
 {
 	return 0;
