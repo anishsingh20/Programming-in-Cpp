0aa84e06e38c55a4b13e816858b86658165e315c
anishsingh@20 <loveeanish@gmail.com>
2018-05-23 15:04:21 +0530

    reading data from file

--- C-practice/Input and Output/IO.c
+++ C-practice/Input and Output/IO.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
 #include<conio.h>
+#include<stdlib.h>
 
 
 
 int main()
 {
-	char ch[20] = "Anish Walia";
-	char *fp = fopen("a.txt","r+");//open the file in write mode
 	
-	fputs(ch,fp); //writing the the file
+	FILE *fp = fopen("a.txt","r");//open the file in write mode
 	
-	char chr[30];
+//	fputs(ch,fp); //writing the the file
 	
-	while(ch = fgets(strlen(fp)) != EOF)
+	char chr[20];
+	
+	if( fgets(chr,20,fp) != NULL)
 	{
-		
+		fseek(fp,3L,5);
+		puts(chr);	
 	}
+	
+	fclose(fp);
 	
 	return 0;
 }
