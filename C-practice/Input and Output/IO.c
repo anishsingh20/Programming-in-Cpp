f04948c21e2321a3f50ec5d8364ca12f70bab7c8
anishsingh@20 <loveeanish@gmail.com>
2018-05-23 21:28:38 +0530

    usage of SEEK end

--- C-practice/Input and Output/IO.c
+++ C-practice/Input and Output/IO.c
@@ -11,14 +11,14 @@ int main()
 
 
 	char ch[10];
-	fputs(ch,fp);
-	fseek(fp,6L,SEEK_CUR);
+	fseek(fp,-5L,SEEK_END);
 	fgets(ch,5,fp);//ie store 4 items from the 6 digit in file a.txt 
 	
 	/*It stops when either (n-1) characters are read, the newline character is read, or the end-of-file is reached,
 	 whichever comes first
 	 */
 	puts(ch);
+	
 	
 	return 0;
 }
