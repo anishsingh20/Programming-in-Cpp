9d07e063c570abdfa2bfa1e14fa623c7d34609c0
anishsingh@20 <loveeanish@gmail.com>
2018-05-23 15:08:31 +0530

    successfully reading data from file in C

--- C-practice/Input and Output/IO.c
+++ C-practice/Input and Output/IO.c
@@ -7,15 +7,14 @@
 int main()
 {
 	
-	FILE *fp = fopen("a.txt","r");//open the file in write mode
-	
-//	fputs(ch,fp); //writing the the file
+	FILE *fp = fopen("a.txt","r+");//open the file in write mode
+
+
 	
 	char chr[20];
 	
 	if( fgets(chr,20,fp) != NULL)
 	{
-		fseek(fp,3L,5);
 		puts(chr);	
 	}
 	
