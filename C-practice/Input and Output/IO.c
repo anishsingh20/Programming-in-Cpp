b3ee767eb1a6b5f10c4f024b0ac92e9559ad2213
anishsingh@20 <loveeanish@gmail.com>
2018-05-23 21:36:22 +0530

    another program implemented on C IO

--- C-practice/Input and Output/IO.c
+++ C-practice/Input and Output/IO.c
@@ -7,18 +7,23 @@
 int main()
 {
 	
-	FILE *fp = fopen("a.txt","r");//open the file in write mode
-
-
-	char ch[10];
-	fseek(fp,-5L,SEEK_END);
-	fgets(ch,5,fp);//ie store 4 items from the 6 digit in file a.txt 
+//	FILE *fp = fopen("a.txt","r");//open the file in write mode
+//
+//
+//	char ch[10];
+//	fseek(fp,-5L,SEEK_END);
+//	fgets(ch,5,fp);//ie store 4 items from the 6 digit in file a.txt 
+//	
+//	/*It stops when either (n-1) characters are read, the newline character is read, or the end-of-file is reached,
+//	 whichever comes first
+//	 */
+//	puts(ch);
 	
-	/*It stops when either (n-1) characters are read, the newline character is read, or the end-of-file is reached,
-	 whichever comes first
-	 */
-	puts(ch);
+	//program2
 	
+	printf("%%%%");//will print only 2 
 	
 	return 0;
 }
+
+
