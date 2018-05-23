88deeb544bc8f829b88b22280dac428cb29e13f4
anishsingh@20 <loveeanish@gmail.com>
2018-05-23 21:10:12 +0530

    new program added on File stream

--- C-practice/Input and Output/IO.c
+++ C-practice/Input and Output/IO.c
@@ -7,18 +7,18 @@
 int main()
 {
 	
-	FILE *fp = fopen("a.txt","r+");//open the file in write mode
+	FILE *fp = fopen("a.txt","r");//open the file in write mode
 
 
+	char ch[10];
+	fputs(ch,fp);
+	fseek(fp,6L,SEEK_CUR);
+	fgets(ch,5,fp);//ie store 4 items from the 6 digit in file a.txt 
 	
-	char chr[20];
-	
-	if( fgets(chr,20,fp) != NULL)
-	{
-		puts(chr);	
-	}
-	
-	fclose(fp);
+	/*It stops when either (n-1) characters are read, the newline character is read, or the end-of-file is reached,
+	 whichever comes first
+	 */
+	puts(ch);
 	
 	return 0;
 }
