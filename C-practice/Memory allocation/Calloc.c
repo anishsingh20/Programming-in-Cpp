928a5a433bb519e578096cb4c5b0f01d47d5bde5
Anish Singh Walia <loveeanish@gmail.com>
2018-05-26 22:42:27 +0530

    Compared calloc and malloc

--- C-practice/Memory allocation/Calloc.c
+++ C-practice/Memory allocation/Calloc.c
@@ -3,10 +3,12 @@
  
 int main()
 {
-	int *p;
+	int *p,*q;
 	p = (int*)calloc(20,sizeof(char));
 	//calloc takes 2 args - no of elements, size of each item. It also sets the values to 0 automatically.
 	
-	printf("%d\n",p[3]);//prints 0
+	q=(int*)malloc(20);
 	
+	printf("%d\n",p[3]);//prints 0
+	printf("%d\n",q[3]);
  }
