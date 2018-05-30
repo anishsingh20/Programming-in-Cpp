eb1c267ce724d82180d732c1a930cf59603ac34f
anishsingh@20 <loveeanish@gmail.com>
2018-05-30 21:55:27 +0530

    another program on complex declarations

--- C-practice/Complex Declarations/complex.c
+++ C-practice/Complex Declarations/complex.c
@@ -13,6 +13,11 @@ int main()
 	//but here it prints the size of a long double as i becomes a long double, and sizeof() operator has right to left associativity
 	//so left most is evaluated at last i.e long double
  	printf("%d",sizeof((long double)(double)(char)(int)(float)i)); //prints 16
+ 	
+ 	
+ 	//program 2
+ 	
+ 	
 	
 		
 }
---
+++ C-practice/Complex Declarations/complex2.c
@@ -0,0 +1,25 @@
+//Complext declarations in C
+#include<stdio.h>
+
+typedef void v;
+
+typedef int i;
+
+int main()
+{
+	v fun(i,i);
+	
+	fun(4,3);
+	
+	return 0;
+}
+
+
+v fun(i a,i b)
+{
+	i s = 3; //here i is the type int
+	float i;//i is a variable here
+	printf("%d\n",sizeof(i)); //prints size of a float i
+	
+	printf("%d",a*b*s);
+}
