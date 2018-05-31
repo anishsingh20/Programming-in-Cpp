f643e6c3363db73082ae8a3d66ca83547dd3a842
anishsingh@20 <loveeanish@gmail.com>
2018-05-31 21:35:33 +0530

    the function made

--- Data Structures/Trees/BST/BSTfromPreorder.cpp
+++ Data Structures/Trees/BST/BSTfromPreorder.cpp
@@ -15,9 +15,46 @@ struct node {
 };
 
 
-node *PreorderToBSTStack(node *root,int pre[])
+struct node *newNode(int data)
 {
+	node *temp = new node();
+	temp->data = data;
+	temp->left = temp->right = NULL;
 	
+	return temp;
+}
+
+node *PreorderToBSTStack(int pre[],int size)
+{
+	//first element in preorder is the root of tree
+	node *root = newNode(pre[0]);
+	
+	stack<node *>s(size); //creating a stack
+	
+	s.push(root);
+	
+	struct node *temp;
+	
+	for(int i=1; i < size ; i++ )
+	{
+		temp = NULL;
+		while(!s.empty() && pre[i] > s.top())
+		{
+			temp = s.top();
+			s.pop()
+			
+		}
+		
+		if(temp!=NULL)
+		{
+			
+			temp->right = newNode(pre[i]);
+			s.push(temp->right);	
+		}	
+			
+	}
+
+
 }
 
 
