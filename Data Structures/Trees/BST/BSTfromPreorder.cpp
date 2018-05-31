e1d2503a5390f51dbb5c9d6e12af72ae701da8e2
anishsingh@20 <loveeanish@gmail.com>
2018-05-31 21:26:08 +0530

    declared the function to convert preorder to BST

--- Data Structures/Trees/BST/BSTfromPreorder.cpp
+++ Data Structures/Trees/BST/BSTfromPreorder.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stack>
 
 /*if we want to construct a tree from given traversals-
 
@@ -8,3 +9,29 @@ Method1)
 1)Using a stack
 */
 
+struct node {
+	int data;
+	struct node *left,*right;
+};
+
+
+node *PreorderToBSTStack(node *root,int pre[])
+{
+	
+}
+
+
+void Inorder(node *root)
+{
+	Inorder(root->left);
+	
+	cout<<root->data;
+	
+	Inorder(root->right);
+}
+
+
+int main()
+{
+	return 0;
+}
