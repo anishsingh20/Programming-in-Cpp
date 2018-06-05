e80d13f5eab0ad518f97ca41d1918d732eb594c9
anishsingh@20 <loveeanish@gmail.com>
2018-06-05 13:13:24 +0530

    made all the helper function

--- Data Structures/LinkedLists/RemoveCycleLinkedList.cpp
+++ Data Structures/LinkedLists/RemoveCycleLinkedList.cpp
@@ -23,6 +23,21 @@ ListNode *newNode(int data)
 
 int main()
 {
+	struct ListNode *head = newNode(1);
+	
+	struct ListNode *n1 = newNode(2);
+	
+	struct ListNode *n2 = newNode(0);
+	
+	struct ListNode *n3  = newNode(4);
+//	
+	
+	head->next = n1;
+	n1->next = n2;
+	
+	n2->next = n3;
+	
+	n3->next = n2;
 	return 0;
 	
 }
