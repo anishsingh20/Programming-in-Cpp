9d88151af81ecb27a92fd4ed8d992e34bf22fd15
anishsingh@20 <loveeanish@gmail.com>
2018-06-06 01:05:54 +0530

    method to remove a cycle from list

--- Data Structures/LinkedLists/RemoveCycleLinkedList.cpp
+++ Data Structures/LinkedLists/RemoveCycleLinkedList.cpp
@@ -5,6 +5,19 @@ using namespace std;
 
 //program to detect a cycle in linked list and remove it.
 
+/*
+1)Method1
+We can easily use Hashing or Visited node techniques to get the pointer to the last node. 
+Idea is simple: the very first node whose next is already visited (or hashed) is the last node.
+If such a node is found then we can set this last node's next to NULL
+
+2)Method 2- Floyd's cycle detection algorithm
+ a)Make a function to return the starting node of the cycle using FLoyd's cycle ddtection algo using fast and slow pointers or by using
+ hash map(unordered_set).
+ b)Then make another function to remove cycle i.e make the last node of cycle point to NULL starting from the start of cycle.
+*/
+
+
 struct ListNode {
 	
 	int val;
@@ -19,6 +32,12 @@ ListNode *newNode(int data)
 	temp->val = data;
 	
 	return temp;
+}
+
+
+bool RemoveCycle(ListNode *head)
+{
+	
 }
 
 int main()
