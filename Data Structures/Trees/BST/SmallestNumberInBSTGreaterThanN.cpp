f6d3c8d2d6e9f09ab73e394c9f5f09faf504a2e2
anishsingh@20 <loveeanish@gmail.com>
2018-06-17 22:42:55 +0530

    program to find smallest number in BST greater than N

---
+++ Data Structures/Trees/BST/SmallestNumberInBSTGreaterThanN.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+/*program to find smallest number in BST greater than equal to N
+1)Method-1 Using level order traversal.
+*/
+
+
+int main()
+{
+	return 0;
+}
