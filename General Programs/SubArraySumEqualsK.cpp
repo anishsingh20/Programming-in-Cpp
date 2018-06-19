a25297f631c7b9422fbf4a897b28727cbc6ea8ac
anishsingh@20 <loveeanish@gmail.com>
2018-06-19 14:09:47 +0530

    another itearative solution using 2 loops

--- General Programs/SubArraySumEqualsK.cpp
+++ General Programs/SubArraySumEqualsK.cpp
@@ -1,34 +1,61 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 //program to find number of  subarrays whose sum equals K
 
 //brute force appraoch
+//int subarraySum(vector<int>& nums, int k) {
+//        
+//       
+//       int count = 0 ; 
+//        for(int i = 0 ; i < nums.size() ; i++)
+//        {
+//            for(int j = i+1 ; j < nums.size(); j++)
+//            {
+//                int sum = 0 ;
+//                for(int m = i ; m < j ; m++)
+//                {
+//                    sum += nums[m];
+//                    
+//                    if(sum == k)
+//                    	count++;    	
+//                }
+//            }
+//            
+//        }
+//        
+//        return count;
+//}
+
 int subarraySum(vector<int>& nums, int k) {
         
-       
-       int count = 0 ; 
-        for(int i = 0 ; i < nums.size() ; i++)
+      int count = 0 ;
+        
+        for(int i = 0 ; i < nums.size(); i++)
         {
-            for(int j = i+1 ; j < nums.size(); j++)
+            int sum = 0 ;
+            for(int j = i ; j < nums.size(); j++)
             {
-                int sum = 0 ;
-                for(int m = i ; m < j ; k++)
-                {
-                    sum += nums[m];
-                    if(sum == k)
+                sum += nums[j];
+                if(sum == k)
                         count++;
-                }
             }
-            
         }
         
         return count;
-}
+    }
 
 
 int main()
 {
+	vector<int> v;
+	
+	v.push_back(1);
+	v.push_back(1);
+	v.push_back(1);
+	
+	cout<<subarraySum(v,2);	
 	
 }
