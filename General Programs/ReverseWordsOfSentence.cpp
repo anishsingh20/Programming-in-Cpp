70ca59cfc3dc350ed3d65783b2d94752f56477eb
anishsingh@20 <loveeanish@gmail.com>
2018-06-26 23:19:29 +0530

    program to reverse words in a program

--- General Programs/ReverseWordsOfSentence.cpp
+++ General Programs/ReverseWordsOfSentence.cpp
@@ -10,40 +10,42 @@ using namespace std;
 
 void ReverseWords(string &s)
 {
-	int j = s.size()-1;
-	reverse(s.begin(),s.end());
-	
-	string::iterator it = s.begin();
-	
-	int count  = 0 ;
-	
-	//iterating till first word's end
-	while(*it != ' ')
-	{	
-		it++;
-	}
-		
-		
-	reverse(s.begin(),it);
-	reverse(it , s.end());
-		
-		
-	
-	
-	
+	int start = 0;
+        int i = 0;
 
-	
-	
-	
-	
-	cout<<s;	
+	//to remove spaces in the starting
+       while( s[i] == ' ') s.erase(i, 1);
+        
+        
+        reverse(s.begin(), s.end());
+        
 
-}
+
+        int size = s.size();
+        for(i = 0; i < size - 1; i++){
+        	
+        	//removing empty spaces
+            if( s[i] == ' '){
+                if( i == start ){
+                    s.erase(i--,1);
+                    size--;
+                }
+                else{
+                    reverse(s.begin()+start, s.begin()+i);
+                    start = i+1;
+                }
+            }
+        }
+        
+        reverse(s.begin() + start, s.end());
+        cout<<s;
+}	
+
 
 int main()
 {
 	
-	string s = "hello world!";
+	string s = " hey  how are you";
 	
 	ReverseWords(s);	
 	return 0;
