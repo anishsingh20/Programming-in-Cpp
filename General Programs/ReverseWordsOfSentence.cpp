25048aa8b2e29466cbf1d2113758bba0b2f123ed
anishsingh@20 <loveeanish@gmail.com>
2018-06-26 13:48:10 +0530

    made some logic to reverse thw words not letters

--- General Programs/ReverseWordsOfSentence.cpp
+++ General Programs/ReverseWordsOfSentence.cpp
@@ -1,12 +1,50 @@
 #include<iostream>
+#include<stack>
+#include<algorithm>
+
 
 using namespace std;
 
 //program to reverse Words of a sentence
 
+
+void ReverseWords(string &s)
+{
+	int j = s.size()-1;
+	reverse(s.begin(),s.end());
+	
+	string::iterator it = s.begin();
+	
+	int count  = 0 ;
+	
+	while(*it != ' ')
+	{	
+		count++;
+		it++;
+	}
+		
+		
+	reverse(s.begin(),s.begin()+count);
+	reverse(s.begin() + count , s.end());
+		
+		
+	
+	
+	
+
+	
+	
+	
+	
+	cout<<s;	
+
+}
+
 int main()
 {
 	
+	string s = "whats up nigga";
 	
+	ReverseWords(s);	
 	return 0;
 }
