a72b0787bc5b6fc10b5c88f3ed6e04f661aabfcd
anishsingh@20 <loveeanish@gmail.com>
2018-06-29 19:24:02 +0530

    program to find maximum in minimum window

---
+++ Dynamic Programming/MaximumInMinimumWindow.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+//program for finding maximum in minimum window of sie 1 to n(size of array)
+
+//Brute force-O(n*n*n) considering each window one by one
+
+
+int main()
+{
+	return 0;
+}
