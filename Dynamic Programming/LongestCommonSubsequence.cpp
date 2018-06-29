1aece9259107763362ac30825dcba0ff11cd8633
anishsingh@20 <loveeanish@gmail.com>
2018-06-30 00:52:14 +0530

    implemented the DP solution which is efficient

--- Dynamic Programming/LongestCommonSubsequence.cpp
+++ Dynamic Programming/LongestCommonSubsequence.cpp
@@ -17,18 +17,26 @@ int LCS_DP(string &a,string &b,int m,int n)
 {
 	int L[m+1][n+1]; //a 2-d table
 	
-	for(int i = 0 ; i  < m ; i++)
+	for(int i = 0 ; i  <= m ; i++)
 	{
-		for(int  j = 0 ; j < n;j++)
+		for(int  j = 0 ; j <= n;j++)
 		{
 			if(i==0 || j==0)
 				L[i][j] = 0;
 			
+			//if same char found-increase the length by 1
 			else if(a[i]==b[j])
+				L[i][j] = L[i-1][j-1] + 1;
+				
+			else L[i][j] = max(L[i-1][j],L[i][j-1]);
 				
 		}
 	}
+	
+	//max length stored in last index
+	return L[m][n];
 }
+//T(n) = O(mn) which is very efficient
 
 
 
@@ -38,7 +46,7 @@ int LCS(string &a,string &b,int n,int m)
 {
 	
 	
-	base case when we reach start of strings
+	//base case when we reach start of strings
 	if(m==0 || n==0)
 		return 0;
 	
@@ -67,5 +75,7 @@ int main()
 	int m = b.size();
 	
 	cout<<LCS(a,b,n,m);
+	cout<<endl;
+	cout<<LCS_DP(a,b,n,m);
 	return 0;
 }
