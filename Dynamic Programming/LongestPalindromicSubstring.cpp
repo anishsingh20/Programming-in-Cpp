b80697ea3e7d5fcb25c16765bef6f4133433a9c8
anishsingh@20 <loveeanish@gmail.com>
2018-07-02 02:13:40 +0530

    implemented the Dp solution using 2-D table

--- Dynamic Programming/LongestPalindromicSubstring.cpp
+++ Dynamic Programming/LongestPalindromicSubstring.cpp
@@ -1,6 +1,44 @@
 #include<iostream>
+#include<string.h>
+
 
 using namespace std;
+
+
+//Brute force , Dynamic programming using a 2-D boolean matrix which keeps true or false for substring
+ 
+string longestPalindrome(string s) {
+        
+   int n = s.length();
+  int longestBegin = 0;
+  int maxLen = 1;
+  bool table[1000][1000] = {false};
+  for (int i = 0; i < n; i++) {
+    table[i][i] = true;
+  }
+  for (int i = 0; i < n-1; i++) {
+    if (s[i] == s[i+1]) {
+      table[i][i+1] = true;
+      longestBegin = i;
+      maxLen = 2;
+    }
+  }
+  for (int len = 3; len <= n; len++) {
+    for (int i = 0; i < n-len+1; i++) {
+      int j = i+len-1;
+      if (s[i] == s[j] && table[i+1][j-1]) {
+        table[i][j] = true;
+        longestBegin = i;
+        maxLen = len;
+      }
+    }
+  }
+  return s.substr(longestBegin, maxLen);
+	
+	
+       
+}
+//T(n) = O(n*n)
 
 bool isPalindrome(string s)
 {
@@ -37,7 +75,7 @@ int LongestPalindromicSubstring(string a)
 		
 		}
 	}
-	
+		
 	return maxCount;
 }
 
@@ -71,8 +109,10 @@ string PrintLongestPalindromicSubstring(string a)
 
 int main()
 {
-
-	cout<<LongestPalindromicSubstring("n");
+	string s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+	cout<<PrintLongestPalindromicSubstring(s);
+	cout<<endl;
+	cout<<LPS_DP(s);
 
 
 	return 0;
