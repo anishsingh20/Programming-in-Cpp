3c3f2a86b7d204ce0780d30a4aacd16ff5e95c5d
anishsingh@20 <loveeanish@gmail.com>
2018-07-01 10:44:01 +0530

    made a recursive function to find LCS

--- Dynamic Programming/longestPalindromicSubsequence.cpp
+++ Dynamic Programming/longestPalindromicSubsequence.cpp
@@ -5,3 +5,21 @@ using namespace std;
 /*Finding longest length of palindromic subsequence using LCS
 
 */
+
+//Naive recursive approach to find LCS
+int LCS(string a,string reva,int n,int m)
+{
+	//base case
+	if(n==0 || m==0)
+		return 0;
+		
+	else if(a[n-1]==reva[m-1])
+		return 1 + LCS(a,reva,n-1,m-1);
+		
+	else return max(LCS(a,reva,n-1,m),LCS(a,reva,n,m-1));
+}
+
+
+
+//function to find length of longest palindromic subseq where we reverse the string and pass it to LCS function
+int LPS()
