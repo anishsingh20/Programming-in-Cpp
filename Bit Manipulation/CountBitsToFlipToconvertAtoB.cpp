0857a8b20bb904e122abeccecdc3fd2c3719ea3c
anishsingh@20 <loveeanish@gmail.com>
2018-07-05 10:44:23 +0530

    implemented program to count bits to flip

--- Bit Manipulation/CountBitsToFlipToconvertAtoB.cpp
+++ Bit Manipulation/CountBitsToFlipToconvertAtoB.cpp
@@ -4,9 +4,27 @@
 using namespace std;
 
 //count number of bits to be flipped in a to convert it to b
+int CountFlip(int a ,int b)
+{
+	int XOR = a^b; //XOR of a and b will be 1 whereever a and b bits are different
+	
+	
+	int count= 0 ;
+	while(XOR)
+	{
+		//counting number of set bits which is equal to number of bits to flip in a
+		if(XOR & 1) count++;
+		XOR /= 2;
+		
+	
+	}
+	
+	return count;
+}
 
 int main()
 {
+	cout<<CountFlip(7,10);
 	
 	return 0;
 }
