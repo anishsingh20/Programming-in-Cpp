732a06c558bdb3f0d399270bb5fc1fe3b975481e
anishsingh@20 <loveeanish@gmail.com>
2018-07-06 11:41:03 +0530

    made the function to pirnt level order line by line

--- Data Structures/Trees/Binary Trees/LevelOrderLineByLine.cpp
+++ Data Structures/Trees/Binary Trees/LevelOrderLineByLine.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 
 using namespace std;
 
@@ -8,4 +9,61 @@ struct node{
 	struct node *left,*right;
 };
 
-void PrintLevelLinebyLine()
+node *newNode(int data)
+{
+	node *temp = new node();
+	temp->data = data;
+	
+	temp->left = temp->right = NULL;
+}
+
+void PrintLevelLinebyLine(node *root)
+{
+	queue<node *>q;
+	
+	if(!root) return ;
+	
+	q.push(root);
+	q.push(NULL); //marker for completion of a level
+	while(!q.empty())
+	{
+		node *temp = q.front();
+		q.pop();
+	
+		if(temp)
+		{
+			cout<<temp->data<<" ";
+			
+			if(temp->left)	q.push(temp->left);
+		
+			if(temp->right)	q.push(temp->right);
+		}	
+		
+		//if a level is completed
+		else if(temp==NULL)
+		{
+			if(!q.empty())	q.push(NULL);
+			
+			//end the line and go to the next line-once a level is finished 
+			cout<<endl;
+		}
+		
+		
+		
+		
+	}
+}
+
+int main(){
+	
+	node *root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->left = newNode(6);
+	root->right->right = newNode(7);
+	
+	PrintLevelLinebyLine(root);
+	
+}
