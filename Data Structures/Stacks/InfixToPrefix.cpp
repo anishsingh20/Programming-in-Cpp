87437f04b98dd79a35b9f7e89a5ad445b3db8ca1
anishsingh@20 <loveeanish@gmail.com>
2018-07-09 14:57:30 +0530

    written the algorithm

--- Data Structures/Stacks/InfixToPrefix.cpp
+++ Data Structures/Stacks/InfixToPrefix.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 
 
+/*Algorithm-
+1) Reverse the given infix expression.
+2) Obtain the postfix expression of the modified infix expression.
+3) Now reverse the postfix expression obtained, which will give us the Prefix expression.
+*/
 using namespace std;
 
 int main()
