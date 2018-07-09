7e6e62e8819dffbe37d2d893b52638479bdc596a
anishsingh@20 <loveeanish@gmail.com>
2018-07-09 15:21:35 +0530

    made the function to convert infix to postfix expression

--- Data Structures/Stacks/InfixToPrefix.cpp
+++ Data Structures/Stacks/InfixToPrefix.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-
+#include<stack>
+using namespace std;
 
 /*Algorithm-
 1) Reverse the given infix expression.
@@ -21,20 +22,68 @@ int getPriority(char c)
 		
 	else if(c=='^')
 		return 3;
+		
+	return 0;
 }
 
 
 string InfixToPostfix(string infix)
 {
 	infix = '(' + infix + ')';
-	string output;
+	string output="";
 	int len = infix.size();
+	stack<char>s;
 	
+	for(int i = 0 ; i < len; i++)
+	{
+		//if operand-add it to the postfix expression
+		if(isalpha(infix[i]) || isdigit(infix[i]))
+			output += infix[i];
+			
+		//if opening brace then push it to stack
+		else if(infix[i]=='(')
+			s.push(infix[i]);
+			
+		else if(infix[i]==')')
+		{
+			while(s.top()!='(')
+			{
+				output += s.top();
+				s.pop();
+			}
+			
+			//remove ( from stack
+			s.pop();
+		}
+		
+		//if operator is found
+		else
+		{
+		
+			if(isOperator(s.top()))
+			{
+				while( getPriority(infix[i]) <= getPriority(s.top()))
+				{
+					output += s.top();
+					s.pop();
+				}
+				
+				//push current lower priority operator on stack
+				s.push(infix[i]);
+					
+			}	
+		}
+			
+	}
+	
+	return output;
 	
 }
-using namespace std;
+
 
 int main()
 {
+	string s= InfixToPostfix("A*B+C/D");
+	cout<<s;
 	return 0;
 }
