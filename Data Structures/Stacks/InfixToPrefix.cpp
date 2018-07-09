7178fbf58219b939c3e57739fd57a3ff5edc621f
anishsingh@20 <loveeanish@gmail.com>
2018-07-09 15:05:35 +0530

    made the helper function

--- Data Structures/Stacks/InfixToPrefix.cpp
+++ Data Structures/Stacks/InfixToPrefix.cpp
@@ -6,6 +6,32 @@
 2) Obtain the postfix expression of the modified infix expression.
 3) Now reverse the postfix expression obtained, which will give us the Prefix expression.
 */
+
+bool isOperator(char c)
+{
+	return (!isdigit(c) && isalpha(c));
+}
+
+int getPriority(char c)
+{
+	if(c=='+' || c=='-')
+		return 1;
+	else if(c=='*' || c=='/')
+		return 2;
+		
+	else if(c=='^')
+		return 3;
+}
+
+
+string InfixToPostfix(string infix)
+{
+	infix = '(' + infix + ')';
+	string output;
+	int len = infix.size();
+	
+	
+}
 using namespace std;
 
 int main()
